8989a0d91dbb4418fd52ca84ef36886030951d68
Pradeep Kumar Ragu Chanthar <pradeepkumar.ragu@gmail.com>
2017-12-04 16:37:09 -0500

    Add files via upload

--- WebServices/JSONData/Restfulapi.php
+++ WebServices/JSONData/Restfulapi.php
@@ -9,13 +9,13 @@ function get_user_by_id($id)
   // make a call in db later.
   switch ($id){
     case 1:
-      $user_info = array("query_type" => "find_glycogene", "mass" => "1056.4", "glycan_type" => "N-linked","organism"=>"mus musculus"); //Query component
+      $user_info = array("query_type" => "find_glycogene", "mass" => "1056.4", "glycan_type" => "N-linked","organism"=>"mus musculus","GlyGenID"=>"G89290XU","Motif_list"=>"motif"); //Query component
       break;
     case 2:
-      $user_info = array("query_type" => "find_glycan", "mass" => "1896.4", "glycan_type" => "N-linked","organism"=>"mus musculus");
+      $user_info = array("query_type" => "find_glycan", "mass" => "1896.4", "glycan_type" => "N-linked","organism"=>"mus musculus","GlyGenID"=>"G82348BZ","Motif_list"=>"motif");
       break;
     case 3:
-      $user_info = array("query_type" => "find_glycogen", "mass" => "2056.4", "glycan_type" => "N-linked","organism"=>"mus musculus");
+      $user_info = array("query_type" => "find_glycogen", "mass" => "2056.4", "glycan_type" => "N-linked","organism"=>"mus musculus","GlyGenID"=>"G823499Z","Motif_list"=>"motif");
       break;
   }
 
