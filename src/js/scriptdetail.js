2a50b005e5ff3526d7ed8c9cb2e96440139f836e
RupaliM <33363853+RupaliMahadik@users.noreply.github.com>
2018-02-19 12:03:30 -0500

    detail script
    minimum json display

--- src/js/scriptdetail.js
+++ src/js/scriptdetail.js
@@ -1,4 +1,9 @@
 
+   //@author: Rupali Mahadik 
+// @description: UO1 Version-1.1.
+//@Date:19th Feb 2018.
+
+
     function LoadDetails(id) {
       var template = $('#item_template').html();
       var serviceUrl = "http://glycomics.ccrc.uga.edu/ggtest/service/detailpage.php?action=get_user&id=" + id;
@@ -8,17 +13,39 @@
         // data from the service
         console.log(data);
 
-        data.hasMotifs = (data.motifs.length > 0);
+        data.hasMotifs = (data.motifs && (data.motifs.length > 0));
+        data.hasGlycosylate = (data.glycosylate && (data.glycosylate.length > 0));
 
         // data after changes
         console.log(data);
 
         // I'm just replacing all the content in #content. The template below is just to get one var from the data set
         var html = Mustache.to_html(template, data);
+        var $container = $('#content');
 
-        $('#content').html(html);
+        $container.html(html);
+
+        $container.find('.open-close-button').each(function (i, element) {
+          $(element).on('click', function () {
+            var $this = $(this);
+            var buttonText = $this.text();
+
+            if (buttonText === '+') {
+              $this.text('-');
+              $this.parent().next().show();
+            } else {
+              $this.text('+');
+              $this.parent().next().hide();
+              // this.parentNode
+              // parent.parentNode.children
+              // children.indexOf(this)
+              // children[index+1]
+            }
+          });
+        });
       });
     }
+
 
     function getParameterByName(name, url) {
       if (!url) url = window.location.href;
@@ -34,6 +61,3 @@
       var id = getParameterByName('id');
       LoadDetails(id);
     });
-
-    
- 
