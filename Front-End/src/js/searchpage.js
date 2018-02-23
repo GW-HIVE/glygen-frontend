278e30fd0432cc5b3aea39716676dd4978bdf445
Pradeep Kumar Ragu Chanthar <pradeepkumar.ragu@gmail.com>
2018-02-23 12:45:18 -0500

    Add files via upload

---
+++ Front-End/src/js/searchpage.js
@@ -0,0 +1,15 @@
+    function myFunction(){
+        var data = {
+         //   formvalues: document.getElementById("myForm")
+           Mass: document.getElementById("Mass"),
+            Glycan_type:  document.getElementById("Glycan_type"),
+            Glycan_subtype: document.getElementById("Glycan_subtype"),        
+            Organism:  document.getElementById("Organism"),
+            ProteinID:  document.getElementById("ProteinID"),
+            Enzyme:  document.getElementById("Enzyme"),
+            GlycanMotif:  document.getElementById("GlycanMotif"),
+            GlycanID:  document.getElementById("GlycanID")
+        };
+   // var formData = JSON.stringify($("#myForm").serializeArray());
+    $.post("http://glycomics.ccrc.uga.edu/ggtest/service/searchpage.php", {formvalues: data},function(response){});
+    }
