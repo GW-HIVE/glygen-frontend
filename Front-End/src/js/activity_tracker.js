3e38846fc73717149055b29434c7583cff632f0b
glagarwal <agarwalgaurav1995@gmail.com>
2018-02-27 05:17:25 -0500

    Logging activity permission banner

--- Front-End/src/js/activity_tracker.js
+++ Front-End/src/js/activity_tracker.js
@@ -1,45 +1,48 @@
 function tracking() {
     var txt = '';
+    var track_banner = document.getElementById("tracking_banner");
+    track_banner.style.display = "none";
     if (typeof (Storage) !== "undefined") {                  // Check browser support
         if (localStorage.getItem("ID") == "NO") {             // if user selected not to be tracked.
             var changeTo = "log";
             txt = "You have chosen NOT to record your activity.\nWant to -> <button onclick='clearLocalStore(\""+changeTo+"\")'>Start Logging</button>";
-            displayMsg(txt);
+            displayBannerMsg(txt);
         }
         else if (localStorage.getItem("ID")) {                // if an ID exists other than "NO" so continue Logging activity.
             var changeTo = "NoLog";
             txt = "Your activity on this website is being tracked.\nWant to -> <button onclick='clearLocalStore(\""+changeTo+"\")'>Stop Logging</button>";
             txt += "<br/>Your ID is:" + localStorage.getItem("ID");
-            displayMsg(txt);
+            displayBannerMsg(txt);
             activityTracker();                              //pass the stored ID to web service
         }
         else {                                               // If nothing in the local storage then give the user a choice.
-            var choice = "Do you want to allow us to track your usage on this website?\n";
-            choice += "<button onclick='logID()'>Yes</button> <button onclick='doNotLog()'>No</button>";
-            document.getElementById("result").innerHTML = choice;
+            track_banner.style.display = "block";
+            document.getElementById("close_banner").onclick = function() {
+                track_banner.style.display = "none";
+            }
         }
     } else {
         txt = "Sorry, your browser does not support Web Storage... Please update it to the latest version";
-        displayMsg(txt);
+        displayBannerMsg(txt);
     }
 }
 
 function logID() {
-    var txt = "Good choice!";
+    var txt = "Good choice! You can change this setting at any time under <strong>My GlyGen</strong> tab.";
     var data = "No ID assigned";
     $.get(getWsUrl("generate_ID")+"?action=get_id", function (response) {              //$.get("http://localhost:8080/Logging.php?action=get_id", function(response) {
         data = response.user_id;
         localStorage.setItem("ID", data);                           //Store the ID from the webservice
-        txt += "<br/>Your ID is:" + localStorage.getItem("ID");     // Retrieve ID from localStorage
-        displayMsg(txt);
+        txt += " Your ID is:" + localStorage.getItem("ID");     // Retrieve ID from localStorage
+        displayBannerMsg(txt);
         activityTracker();
     });
 }
 
 function doNotLog() {
     localStorage.setItem("ID", "NO");
-    var txt = "You chose not to be tracked!";
-    displayMsg(txt);
+    var txt = "You <strong>Declined</strong> the offer! However, you can change this setting at any time under <strong>My GlyGen</strong> tab.";
+    displayBannerMsg(txt);
 }
 
 function clearLocalStore(changeTo) {
@@ -50,8 +53,11 @@ function clearLocalStore(changeTo) {
         doNotLog();
 }
 
-function displayMsg(txt) {
-    document.getElementById("result").innerHTML = txt;
+function displayBannerMsg(txt) {
+    document.getElementById("tracking_banner").innerHTML = "<span>"+txt+"</span><span id='close_banner' style='float: right'>&times;</span>";
+    document.getElementById("close_banner").onclick = function() {
+        document.getElementById("tracking_banner").style.display = "none";
+    }
 }
 
 function activityTracker() {
