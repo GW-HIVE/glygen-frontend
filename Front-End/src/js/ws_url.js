7ec18328cb3edbc702b8b579b300478f7ebf29ee
glagarwal <agarwalgaurav1995@gmail.com>
2018-04-30 03:57:52 -0400

    Contact form + error activity loging

--- Front-End/src/js/ws_url.js
+++ Front-End/src/js/ws_url.js
@@ -1,51 +1,54 @@
-function getWsUrl(request, id){
-    
-    
-    var ws_base ="http://glygen-vm-prd.biochemistry.gwu.edu/api/glycan";
+function getWsUrl(request, id) {
+
+
+    var ws_base = "http://glygen-vm-prd.biochemistry.gwu.edu/api/glycan";
     var ws_logging = "http://glycomics.ccrc.uga.edu/ggtest/service/Logging.php"; //ws_base+"Logging.php";
     var ws_userID = ws_logging;
-    
-   
+    var ws_contact = ws_base + "/contact.php";
+
+
     switch (request) {
-    case "generate_ID":
-    return ws_userID;
-    break;
-    case "log_activity":
-    return ws_logging;
-    break;
-     case "image_service":
-    return ws_base + "/image/" + id; 
-    break;
-    case "list":
-    return ws_base + "/list";
-    break;
-    case "detail":
-    return ws_base +"/detail/" + id;
-    break;
-    default:
-    return "";
-    break;
+        case "generate_ID":
+            return ws_userID;
+            break;
+        case "log_activity":
+            return ws_logging;
+            break;
+        case "image_service":
+            return ws_base + "/image/" + id;
+            break;
+        case "list":
+            return ws_base + "/list";
+            break;
+        case "detail":
+            return ws_base + "/detail/" + id;
+            break;
+        case "contact":
+            return ws_contact;
+            break;
+        default:
+            return "";
+            break;
     }
-   }
-   
+}
 
 
-   /**
-    * getListPostData  function that returns the string with the correct format the GWU service need to get the list for a specific Id
+
+/**
+ * getListPostData  function that returns the string with the correct format the GWU service need to get the list for a specific Id
  
-     * @param {string} id -  serach ID
-  */
-   
-   function getListPostData(id, page, sort, dir, limit)
-   {
+  * @param {string} id -  serach ID
+*/
+
+function getListPostData(id, page, sort, dir, limit) {
     var query = {};
     query.id = id;
     query.offset = ((page - 1) * limit) + 1;
     query.sort = sort;
     query.limit = parseInt(limit);
     query.order = dir;
-   
+
     return "query=" + JSON.stringify(query);
-   }
-   
+}
+
 
