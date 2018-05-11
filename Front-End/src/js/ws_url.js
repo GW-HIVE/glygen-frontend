27370dc50d188da1dca32bf4752ed9b04789ee88
RupaliM <rupali.mahadik88@gmail.com>
2018-05-11 10:11:39 -0400

    search page redirected with id
    Searchpage:
    Protein mass slider works
    organism selections works
    Protein typeahead works
    serach button redirected to protein-list page.

--- Front-End/src/js/ws_url.js
+++ Front-End/src/js/ws_url.js
@@ -1,7 +1,7 @@
 function getWsUrl(request, id){
         
     var ws_base_glycan ="http://glygen-vm-prd.biochemistry.gwu.edu/api/glycan";
-    var ws_base_protein ="http://glygen-vm-prd.biochemistry.gwu.edu/api/protein";
+    var ws_base_protein ="http://glygen-vm-tst.biochemistry.gwu.edu/api/protein";
     var ws_logging = ws_base_glycan+"/auth/logging";
     var ws_userID = ws_base_glycan+"/auth/userid";
     
@@ -27,10 +27,10 @@ function getWsUrl(request, id){
     case "search_init":
     return ws_base_glycan +"/search_init";
     break;
-    case "typehead":
-            return ws_base_protein +"/typehead";
+    case "typehead_protein":
+            return ws_base_glycan +"/typehead";
             break;
-            case "search_init":
+            case "search_init_protein":
             return ws_base_protein +"/search_init";
             break;
     default:
@@ -58,5 +58,38 @@ function getWsUrl(request, id){
    
     return "query=" + JSON.stringify(query);
    }
-   
 
+   function getSearchtypeheadData(field, value){
+       var query = {};
+       query.field = field;
+       query.value = value;
+       return "query=" + JSON.stringify(query);
+   }
+
+
+
+// function getProteinsearchPostdata(protein_name_long,organism,glycosylation_evidence,pathway_id,protein_id,massmin,massmax,gene_name) {
+//     var query = {};
+//     query.mass = {
+//         min: massmin,
+//         max: massmax
+//     };
+//     query.protein_name_long = protein_name_long;
+//     query.organism = organism ;
+//     query.glycosylation_evidence = glycosylation_evidence;
+//     query.pathway_id = pathway_id;
+//     query.protein_id = protein_id;
+//
+//     query.gene_name = gene_name;
+//     return "query=" + JSON.stringify(query);
+// }
+
+// function getProteinSearchPostdata(organism,protein,mass,gene_name) {
+//     var query = {};
+//     query.mass =  mass;
+//     query.organism = organism ;
+//     query.protein = protein;
+//
+//     query.gene_name = gene_name;
+//     return "query=" + JSON.stringify(query);
+// }
