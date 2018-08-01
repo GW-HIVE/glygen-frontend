1dd33c3c3b7453208198d1bb5c582d2314d41e73
Tatiana <tbw32@hotmail.com>
2018-08-01 12:17:42 -0400

    Updated popup content and examples in sesarch pages

--- Front-End/src/js/glycoprotein_search.js
+++ Front-End/src/js/glycoprotein_search.js
@@ -196,7 +196,7 @@ var mass_min;
 $(document).ready(function () {
     $(".glycosylated_aa").chosen({
         // max_selected_options: 10,
-        placeholder_text_multiple: "choose amino acid"
+        placeholder_text_multiple: "Select and choose amino acid(s)"
     })
         .bind("chosen:maxselected2", function () {
             window.alert("You reached your limited number of selections which is 2 selections!");
