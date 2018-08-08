06c406ec12bd8bfabda5669220bbce5902eee48d
RupaliM <rupali.mahadik88@gmail.com>
2018-08-08 13:03:59 -0400

    Disease url updated /Text chanaged in Glycan list page

--- Front-End/src/js/ws_url.js
+++ Front-End/src/js/ws_url.js
@@ -134,7 +134,7 @@ function getWsUrl(request, id) {
 
         //Usecases DISEASE webservices Q.10
         case "search_disease":
-            return ws_useCaseSearch + "/disease_to_glycosyltransferases/0/0050560/" + id;
+            return ws_useCaseSearch + "/disease_to_glycosyltransferases";
             break;
     }
 }
