ac23b10f9cef0d42e24d89ddf68d2cbae9f38250
glagarwal <agarwalgaurav1995@gmail.com>
2018-09-24 13:54:02 -0400

    Download button on glycan detail page
    https://ccrcbioinf.atlassian.net/browse/GLYG-316

--- Front-End/src/js/utility.js
+++ Front-End/src/js/utility.js
@@ -277,3 +277,14 @@ function decideAjaxError(jqStatus, textStatus){
     }
     return err;
 }
+
+/**
+ * for the pageproofer feedback.
+ */
+(function (d, t) {
+    var pp = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    pp.src = '//app.pageproofer.com/overlay/js/3434/1775';
+    pp.type = 'text/javascript';
+    pp.async = true;
+    s.parentNode.insertBefore(pp, s);
+ })(document, 'script');
