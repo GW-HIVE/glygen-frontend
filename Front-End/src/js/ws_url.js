0489d3cafd7adef3050bc2abdb3ff332c8317bd4
glagarwal <agarwalgaurav1995@gmail.com>
2018-11-02 16:27:01 -0400

    error and warning correction

--- Front-End/src/js/ws_url.js
+++ Front-End/src/js/ws_url.js
@@ -29,61 +29,50 @@ function getWsUrl(request, id, id1) {
         //Auth Webservices.
         case "generate_id":
             return ws_userID;
-            break;
         case "log_activity":
             return ws_logging;
-            break;
         case "contact":
             return ws_contact;
-            break;
         //Homepage webservices
         case "home_init":
             return ws_homeInit;
-            break;
         //Glycan webservices
         case "search_init_glycan":
             return ws_base_glycan + "/search_init";
-            break;
 
         case "glycan_search":
             return ws_base_glycan + "/search";
-            break;
 
         case "glycan_search_simple":
             return ws_base_glycan + "/search_simple";
-            break;
 
         case "glycan_image":
             return ws_base_glycan + "/image/" + id;
-            break;
 
         case "glycan_list":
             return ws_base_glycan + "/list";
-            break;
+
         case "glycan_detail":
             return ws_base_glycan + "/detail/" + id;
-            break;
-
 
         //Protein webservices
         case "search_init_protein":
             return ws_base_protein + "/search_init";
-            break;
+
         case "search_protein":
             return ws_base_protein + "/search";
-            break;
+
         case "protein_search_simple":
             return ws_base_protein + "/search_simple";
-            break;
+
         case "protein_image_service":
             return ws_base_glycan + "/image/" + id;
-            break;
+
         case "protein_list":
             return ws_base_protein + "/list";
-            break;
+
         case "protein_detail":
             return ws_base_protein + "/detail/" + id;
-            break;
 
         //Typeahead webservices
 
@@ -91,59 +80,47 @@ function getWsUrl(request, id, id1) {
             return ws_base_typeahead;
         default:
             return "WWW.GOOGLE.COM";
-            break;
 
         //Usecases search webservices
         //Usecases search webservices Q1-Q2-Q3
         case "search_bioenzyme":
             return ws_useCaseSearch + "/glycan_to_biosynthesis_enzymes/9606/" + id;
-            break;
+
         case "search_glycansite":
             return ws_useCaseSearch + "/glycan_to_glycoproteins/0/" + id;
-            break;
+
         case "search_glycangene":
             return ws_useCaseSearch + "/glycan_to_enzyme_gene_loci/9606/" + id;
-            break;
 
         // usecases list API Q3 and 4
         case "list_glycangene":
             return ws_useCaseSearch + "/ortholog_list";
-            break;
 
         case "loci_list":
             return ws_useCaseSearch + "/genelocus_list";
-            break;
-
-
 
         //Usecases search webservices Q4-Q6
         case "search_proteinorthologues":
             return ws_useCaseSearch + "/protein_to_orthologs/" + id;
-            break;
 
         case "search_glycanenzyme":
             return ws_useCaseSearch + "/biosynthesis_enzyme_to_glycans/10090/" + id;
-            break;
-
 
         //Usecases search webservices Q7-Q8-Q9
 
         case "search_glycosyltransferases":
             return ws_useCaseSearch + "/species_to_glycosyltransferases/" + id;
-            break;
+
         case "search_glycohydrolases":
             return ws_useCaseSearch + "/species_to_glycohydrolases/" + id;
-            break;
+
         case "search_glycoproteins":
             return ws_useCaseSearch + "/species_to_glycoproteins/" + id + "/" + id1;
-            break;
-
 
 
         //Usecases DISEASE webservices Q.10
         case "search_disease":
             return ws_useCaseSearch + "/disease_to_glycosyltransferases";
-            break;
 
         case "data_download":
             return ws_dataDownload;
