d4fe6bea30ffa3fb0c60af6a9a97a3a9d190008e
glagarwal <agarwalgaurav1995@gmail.com>
2018-11-09 16:16:29 -0500

    debug check for pageproofer

--- Front-End/src/js/domain_url_test.js
+++ Front-End/src/js/domain_url_test.js
@@ -12,9 +12,11 @@ var ws_base = "http://tst.api.glygen.org/";
  * for the pageproofer feedback.
  */
 (function (d, t) {
+    debugger;
    var pp = d.createElement(t), s = d.getElementsByTagName(t)[0];
    pp.src = '//app.pageproofer.com/overlay/js/3487/1801';
    pp.type = 'text/javascript';
    pp.async = true;
    s.parentNode.insertBefore(pp, s);
+   debugger;
 })(document, 'script');
