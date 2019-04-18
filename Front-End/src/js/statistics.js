2446f3df37e39ab949a865a668c3bf844b00f612
Tatiana <tbw32@hotmail.com>
2019-04-18 10:52:43 -0400

    Added a simple donut chart

--- Front-End/src/js/statistics.js
+++ Front-End/src/js/statistics.js
@@ -1,155 +1,154 @@
 // @author Tatiana Williamson
 // @createDate March 11, 2019
 
-//d3.json("statistics.json", function(data){
 const width = 500,
-      height = 500,
-      maxRadius = (Math.min(width, height) / 2) - 20;
+	height = 500,
+	maxRadius = (Math.min(width, height) / 2) - 20;
 
 const formatNumber = d3.format(',d');
 
 const x = d3.scaleLinear()
-      .range([0, 2 * Math.PI])
-      .clamp(true);
+	.range([0, 2 * Math.PI])
+	.clamp(true);
 
 const y = d3.scaleSqrt()
-      .range([maxRadius * .1, maxRadius]);
+	.range([maxRadius * .1, maxRadius]);
 
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 
 const partition = d3.partition();
 
 const arc = d3.arc()
-      .startAngle(d => x(d.x0))
-      .endAngle(d => x(d.x1))
-      .innerRadius(d => Math.max(0, y(d.y0)))
-      .outerRadius(d => Math.max(0, y(d.y1)));
+	.startAngle(d => x(d.x0))
+	.endAngle(d => x(d.x1))
+	.innerRadius(d => Math.max(0, y(d.y0)))
+	.outerRadius(d => Math.max(0, y(d.y1)));
 
 const middleArcLine = d => {
-      const halfPi = Math.PI / 2;
-      const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
-      const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+	const halfPi = Math.PI / 2;
+	const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
+	const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
 
-      const middleAngle = (angles[1] + angles[0]) / 2;
-      const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
-      if (invertDirection) {
-            angles.reverse();
-      }
+	const middleAngle = (angles[1] + angles[0]) / 2;
+	const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+	if (invertDirection) {
+		angles.reverse();
+	}
 
-      const path = d3.path();
-      path.arc(0, 0, r, angles[0], angles[1], invertDirection);
-      return path.toString();
+	const path = d3.path();
+	path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+	return path.toString();
 };
 
 const textFits = d => {
-      const CHAR_SPACE = 6;
+	const CHAR_SPACE = 6;
 
-      const deltaAngle = x(d.x1) - x(d.x0);
-      const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
-      const perimeter = r * deltaAngle;
+	const deltaAngle = x(d.x1) - x(d.x0);
+	const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+	const perimeter = r * deltaAngle;
 
-      return d.data.name.length * CHAR_SPACE < perimeter;
+	return d.data.name.length * CHAR_SPACE < perimeter;
 };
 
 const svg = d3.select('.zoomableSunburst')
-      .style('width', '600')
-      .style('height', '600')
-      .attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
-      .on('click', () => focusOn()); // Reset zoom on canvas click
+	.style('width', '500')
+	.style('height', '500')
+	.attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
+	.on('click', () => focusOn()); // Reset zoom on canvas click
 
-//d3.json('https://gist.githubusercontent.com/mbostock/4348373/raw/85f18ac90409caa5529b32156aa6e71cf985263f/flare.json', (error, root) => {
-//    if (error) throw error;
-d3.json("statistics.json", (error, root) => {
-      if (error) throw error;
+d3.json("data/statistics.json", (error, root) => {
+	if (error) throw error;
+	//console.log(root);
 
-      root = d3.hierarchy(root);
-      root.sum(d => d.size);
+	root = d3.hierarchy(root);
+	root.sum(d => d.size);
 
-      const slice = svg.selectAll('g.slice')
-            .data(partition(root).descendants());
+	const slice = svg.selectAll('g.slice')
+		.data(partition(root).descendants());
 
-      slice.exit().remove();
+	slice.exit().remove();
 
-      const newSlice = slice.enter()
-            .append('g').attr('class', 'slice')
-            .on('click', d => {
-                  d3.event.stopPropagation();
-                  focusOn(d);
-            });
+	const newSlice = slice.enter()
+		.append('g').attr('class', 'slice')
+		.on('click', d => {
+			d3.event.stopPropagation();
+			focusOn(d);
+		});
 
-      newSlice.append('title')
-            .text(d => d.data.name + '\n' + formatNumber(d.value));
+	newSlice.append('title')
+		.text(d => d.data.name + '\n' + formatNumber(d.value));
 
-      newSlice.append('path')
-            .attr('class', 'main-arc')
-            .style('fill', d => color((d.children ? d : d.parent).data.name))
-            .attr('d', arc);
+	newSlice.append('path')
+		.attr('class', 'main-arc')
+		.style('fill', d => color((d.children ? d : d.parent).data.name))
+		.attr('d', arc);
 
-      newSlice.append('path')
-            .attr('class', 'hidden-arc')
-            .attr('id', (_, i) => `hiddenArc${i}`)
-            .attr('d', middleArcLine);
+	newSlice.append('path')
+		.attr('class', 'hidden-arc')
+		.attr('id', (_, i) => `hiddenArc${i}`)
+		.attr('d', middleArcLine);
 
-      const text = newSlice.append('text')
-            .attr('display', d => textFits(d) ? null : 'none');
+	const text = newSlice.append('text')
+		.attr('display', d => textFits(d) ? null : 'none');
 
-      // Add white contour
-      text.append('textPath')
-            .attr('startOffset', '50%')
-            .attr('xlink:href', (_, i) => `#hiddenArc${i}`)
-            .text(d => d.data.name)
-            .style('fill', 'none')
-            .style('stroke', '#fff')
-            .style('stroke-width', 5)
-            .style('stroke-linejoin', 'round');
+	// Add white contour
+	text.append('textPath')
+		.attr('startOffset', '50%')
+		.attr('xlink:href', (_, i) => `#hiddenArc${i}`)
+		.text(d => d.data.name)
+		.style('fill', 'none')
+		.style('stroke', '#fff')
+		.style('stroke-width', 5)
+		.style('stroke-linejoin', 'round');
 
-      text.append('textPath')
-            .attr('startOffset', '50%')
-            .attr('xlink:href', (_, i) => `#hiddenArc${i}`)
-            .text(d => d.data.name);
+	text.append('textPath')
+		.attr('startOffset', '50%')
+		.attr('xlink:href', (_, i) => `#hiddenArc${i}`)
+		.text(d => d.data.name);
 });
 
 function focusOn(d = {
-      x0: 0,
-      x1: 1,
-      y0: 0,
-      y1: 1
-}) {
-      // Reset to top-level if no data point specified
+	x0: 0,
+	x1: 1,
+	y0: 0,
+	y1: 1
+})
 
-      const transition = svg.transition()
-            .duration(550)
-            .tween('scale', () => {
-                  const xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-                        yd = d3.interpolate(y.domain(), [d.y0, 1]);
-                  return t => {
-                        x.domain(xd(t));
-                        y.domain(yd(t));
-                  };
-            });
+{
+	// Reset to top-level if no data point specified
+	const transition = svg.transition()
+		.duration(550)
+		.tween('scale', () => {
+			const xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+				yd = d3.interpolate(y.domain(), [d.y0, 1]);
+			return t => {
+				x.domain(xd(t));
+				y.domain(yd(t));
+			};
+		});
 
-      transition.selectAll('path.main-arc')
-            .attrTween('d', d => () => arc(d));
+	transition.selectAll('path.main-arc')
+		.attrTween('d', d => () => arc(d));
 
-      transition.selectAll('path.hidden-arc')
-            .attrTween('d', d => () => middleArcLine(d));
+	transition.selectAll('path.hidden-arc')
+		.attrTween('d', d => () => middleArcLine(d));
 
-      transition.selectAll('text')
-            .attrTween('display', d => () => textFits(d) ? null : 'none');
+	transition.selectAll('text')
+		.attrTween('display', d => () => textFits(d) ? null : 'none');
 
-      moveStackToFront(d);
+	moveStackToFront(d);
 
-      //
+	//
 
-      function moveStackToFront(elD) {
-            svg.selectAll('.slice').filter(d => d === elD)
-                  .each(function (d) {
-                        this.parentNode.appendChild(this);
-                        if (d.parent) {
-                              moveStackToFront(d.parent);
-                        }
-                  })
-      }
+	function moveStackToFront(elD) {
+		svg.selectAll('.slice').filter(d => d === elD)
+			.each(function (d) {
+				this.parentNode.appendChild(this);
+				if (d.parent) {
+					moveStackToFront(d.parent);
+				}
+			})
+	}
 }
 
 //--------------------
@@ -157,140 +156,209 @@ function focusOn(d = {
 //----------------------
 
 var chart = venn.VennDiagram()
-      .width(500)
-      .height(500);
+	.width(500)
+	.height(500);
 
 var div = d3.select("#venn")
 div.datum(sets).call(chart);
 
 var tooltip = d3.select("body").append("div")
-      .attr("class", "venntooltip");
+	.attr("class", "venntooltip");
 
 div.selectAll("path")
-      .style("stroke-opacity", 0)
-      .style("stroke", "#fff")
-      .style("stroke-width", 3)
+	.style("stroke-opacity", 0)
+	.style("stroke", "#fff")
+	.style("stroke-width", 3)
 
 div.selectAll("g")
-      .on("mouseover", function (d, i) {
-            // sort all the areas relative to the current item
-            venn.sortAreas(div, d);
+	.on("mouseover", function (d, i) {
+		// sort all the areas relative to the current item
+		venn.sortAreas(div, d);
 
-            // Display a tooltip with the current size
-            tooltip.transition().duration(400).style("opacity", .9);
-            tooltip.text(d.size + '\n' +  d.tooltipLabel);
+		// Display a tooltip with the current size
+		tooltip.transition().duration(400).style("opacity", .9);
+		tooltip.text(d.size + '\n' + d.tooltipLabel);
 
-            // highlight the current path
-            var selection = d3.select(this).transition("tooltip").duration(400);
-            selection.select("path")
-                  .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
-                  .style("stroke-opacity", 1);
-      })
+		// highlight the current path
+		var selection = d3.select(this).transition("tooltip").duration(400);
+		selection.select("path")
+			.style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+			.style("stroke-opacity", 1);
+	})
 
-      .on("mousemove", function () {
-            tooltip.style("left", (d3.event.pageX) + "px")
-                  .style("top", (d3.event.pageY - 28) + "px");
-      })
+	.on("mousemove", function () {
+		tooltip.style("left", (d3.event.pageX) + "px")
+			.style("top", (d3.event.pageY - 28) + "px");
+	})
 
-      .on("mouseout", function (d, i) {
-            tooltip.transition().duration(400).style("opacity", 0);
-            var selection = d3.select(this).transition("tooltip").duration(400);
-            selection.select("path")
-                  .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
-                  .style("stroke-opacity", 0);
-      });
+	.on("mouseout", function (d, i) {
+		tooltip.transition().duration(400).style("opacity", 0);
+		var selection = d3.select(this).transition("tooltip").duration(400);
+		selection.select("path")
+			.style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+			.style("stroke-opacity", 0);
+	});
 //-----------------------------
 //    Human Venn Diagram
 //-----------------------------
 
 var chart_homo = venn.VennDiagram()
-      .width(400)
-      .height(400);
+	.width(400)
+	.height(400);
 
 var div_homo = d3.select("#venn_homo")
 div_homo.datum(sets_homo).call(chart_homo);
 
 var tooltip = d3.select("body").append("div_homo")
-      .attr("class", "venntooltip");
+	.attr("class", "venntooltip");
 
 div_homo.selectAll("path")
-      .style("stroke-opacity", 0)
-      .style("stroke", "#fff")
-      .style("stroke-width", 3)
+	.style("stroke-opacity", 0)
+	.style("stroke", "#fff")
+	.style("stroke-width", 3)
 
 div_homo.selectAll("g")
-      .on("mouseover", function (d, i) {
-            // sort all the areas relative to the current item
-            venn.sortAreas(div, d);
+	.on("mouseover", function (d, i) {
+		// sort all the areas relative to the current item
+		venn.sortAreas(div, d);
 
-            // Display a tooltip with the current size
-            tooltip.transition().duration(400).style("opacity", .9);
-            tooltip.text(d.size, d.label);
+		// Display a tooltip with the current size
+		tooltip.transition().duration(400).style("opacity", .9);
+		tooltip.text(d.size, d.label);
 
-            // highlight the current path
-            var selection = d3.select(this).transition("tooltip").duration(400);
-            selection.select("path")
-                  .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
-                  .style("stroke-opacity", 1);
-      })
+		// highlight the current path
+		var selection = d3.select(this).transition("tooltip").duration(400);
+		selection.select("path")
+			.style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+			.style("stroke-opacity", 1);
+	})
 
-      .on("mousemove", function () {
-            tooltip.style("left", (d3.event.pageX) + "px")
-                  .style("top", (d3.event.pageY - 28) + "px");
-      })
+	.on("mousemove", function () {
+		tooltip.style("left", (d3.event.pageX) + "px")
+			.style("top", (d3.event.pageY - 28) + "px");
+	})
 
-      .on("mouseout", function (d, i) {
-            tooltip.transition().duration(400).style("opacity", 0);
-            var selection = d3.select(this).transition("tooltip").duration(400);
-            selection.select("path")
-                  .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
-                  .style("stroke-opacity", 0);
-      });
+	.on("mouseout", function (d, i) {
+		tooltip.transition().duration(400).style("opacity", 0);
+		var selection = d3.select(this).transition("tooltip").duration(400);
+		selection.select("path")
+			.style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+			.style("stroke-opacity", 0);
+	});
 
 //-----------------------------
 //    Mouse Venn Diagram
 //-----------------------------
 
 var chart_mus = venn.VennDiagram()
-      .width(400)
-      .height(400);
+	.width(400)
+	.height(400);
 
 var div_mus = d3.select("#venn_mus")
 div_mus.datum(sets_mus).call(chart_mus);
 
 var tooltip = d3.select("body").append("div_mus")
-      .attr("class", "venntooltip");
+	.attr("class", "venntooltip");
 
 div_mus.selectAll("path")
-      .style("stroke-opacity", 0)
-      .style("stroke", "#fff")
-      .style("stroke-width", 3)
+	.style("stroke-opacity", 0)
+	.style("stroke", "#fff")
+	.style("stroke-width", 3)
 
 div_mus.selectAll("g")
-      .on("mouseover", function (d, i) {
-            // sort all the areas relative to the current item
-            venn.sortAreas(div, d);
+	.on("mouseover", function (d, i) {
+		// sort all the areas relative to the current item
+		venn.sortAreas(div, d);
 
-            // Display a tooltip with the current size
-            tooltip.transition().duration(400).style("opacity", .9);
-            tooltip.text(d.size, d.label);
+		// Display a tooltip with the current size
+		tooltip.transition().duration(400).style("opacity", .9);
+		tooltip.text(d.size, d.label);
 
-            // highlight the current path
-            var selection = d3.select(this).transition("tooltip").duration(400);
-            selection.select("path")
-                  .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
-                  .style("stroke-opacity", 1);
-      })
+		// highlight the current path
+		var selection = d3.select(this).transition("tooltip").duration(400);
+		selection.select("path")
+			.style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+			.style("stroke-opacity", 1);
+	})
 
-      .on("mousemove", function () {
-            tooltip.style("left", (d3.event.pageX) + "px")
-                  .style("top", (d3.event.pageY - 28) + "px");
-      })
+	.on("mousemove", function () {
+		tooltip.style("left", (d3.event.pageX) + "px")
+			.style("top", (d3.event.pageY - 28) + "px");
+	})
 
-      .on("mouseout", function (d, i) {
-            tooltip.transition().duration(400).style("opacity", 0);
-            var selection = d3.select(this).transition("tooltip").duration(400);
-            selection.select("path")
-                  .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
-                  .style("stroke-opacity", 0);
-      });
+	.on("mouseout", function (d, i) {
+		tooltip.transition().duration(400).style("opacity", 0);
+		var selection = d3.select(this).transition("tooltip").duration(400);
+		selection.select("path")
+			.style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+			.style("stroke-opacity", 0);
+	});
+
+//-------------------------------
+//    Donut Chart
+//-------------------------------
+//var chart_mus = venn.VennDiagram()
+//	.width(400)
+//	.height(400);
+//
+//var div_mus = d3.select("#venn_mus")
+//div_mus.datum(sets_mus).call(chart_mus);
+//--------------------------------------------
+
+var donut = donutChart()
+	
+function donutChart ()
+{
+    d3.json("data/donut.json", function (error, data)
+    {	
+		if (error) throw error;
+		
+        var radius = 150;
+        var color = d3.scaleOrdinal()
+			.range(["#ff7f0e", "#1f77b4", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#677002", "#17becf"]);
+
+        var canvas = d3.select("#donut_chart")
+            .append("svg")
+            .attr("width", 400)
+            .attr("height", 320);
+
+        var group = canvas.append("g")
+            .attr("transform", "translate(180,150)");
+
+        var arc = d3.arc()
+            .innerRadius(50)
+            .outerRadius(radius);
+
+        var pie = d3.pie()
+            .value(function (d)
+            {
+                return d.rank;
+            });
+
+        var theArc = group.selectAll(".arc")
+            .data(pie(data))
+            .enter()
+            .append("g")
+            .attr("class", "arc");
+
+        theArc.append("path")
+            .attr("d", arc)
+            .attr("fill", function (d)
+            {
+                return color(d.data.rank);
+            });
+
+        theArc.append("text")
+            .attr("transform", function (d)
+            {
+                return "translate(" + arc.centroid(d) + ")";
+            })
+            .attr("dy", "0.15em")
+            .text(function (d)
+            {
+                return d.data.rank;
+            });
+    });
+}
+
+
