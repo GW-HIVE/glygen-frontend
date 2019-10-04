1b616c253451d1ebd9a0cc4c7e1737535ca6f8ed
Tatiana <tbw32@hotmail.com>
2019-10-04 16:20:29 -0400

    Glycansunburst
    Unfinished

--- Front-End/src/js/charts.js
+++ Front-End/src/js/charts.js
@@ -74,6 +74,158 @@ function vennGlycanHomoMusRat(dummy, data, id) {
  * Sunburst glycan chart displayes glycan types and subtypes.
  */
 function sunburstGlycanTypeSubtype(dummy, data, id) {
+	const width = 200,
+	height = 200,
+	maxRadius = (Math.min(width, height) / 2) - 5,
+	margin = {
+		top: 10,
+		right: 10,
+		bottom: 10,
+		left: 10
+	};
+
+const formatNumber = d3.format(',d');
+
+const x = d3.scaleLinear()
+	.range([0, 2 * Math.PI])
+	.clamp(true);
+
+const y = d3.scaleSqrt()
+	.range([maxRadius * .1, maxRadius]);
+
+const color = d3.scaleOrdinal(d3.schemeCategory20);
+
+const partition = d3.partition();
+
+const arc = d3.arc()
+	.startAngle(d => x(d.x0))
+	.endAngle(d => x(d.x1))
+	.innerRadius(d => Math.max(0, y(d.y0)))
+	.outerRadius(d => Math.max(0, y(d.y1)));
+
+const middleArcLine = d => {
+	const halfPi = Math.PI / 2;
+	const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
+	const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+
+	const middleAngle = (angles[1] + angles[0]) / 2;
+	const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+	if (invertDirection) {
+		angles.reverse();
+	}
+
+	const path = d3.path();
+	path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+	return path.toString();
+};
+
+const textFits = d => {
+	const CHAR_SPACE = 1;
+
+	const deltaAngle = x(d.x1) - x(d.x0);
+	const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+	const perimeter = r * deltaAngle;
+
+	return d.data.name.length * CHAR_SPACE < perimeter;
+};
+
+const svg = d3.select(id)
+			.append('svg')
+//            .style('width', '100vw')
+//            .style('height', '100vh')
+.attr('preserveAspectRatio', 'xMinYMin meet')
+	.attr('viewBox',
+		'0 0 ' +
+		(width + margin.left + margin.right) +
+		' ' +
+		(height + margin.top + margin.bottom)
+	)
+	.attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
+	.on('click', () => focusOn()); // Reset zoom on canvas click
+	
+d3.json("data/statistics.json", (error, data) => {
+    if (error) throw error;
+
+    var root = d3.hierarchy(data.sunburst_glycan_type_subtype);
+    root.sum(d => d.size);
+
+    const slice = svg.selectAll('g.slice')
+		.data(partition(root).descendants());
+
+	slice.exit().remove();
+
+    const newSlice = slice.enter()
+		.append('g').attr('class', 'slice')
+		.on('click', d => {
+			d3.event.stopPropagation();
+			focusOn(d);
+		});
+
+    newSlice.append('title')
+		.text(d => d.data.name + '\n' + formatNumber(d.value));
+
+	newSlice.append('path')
+		.attr('class', 'main-arc')
+		.style('fill', d => color((d.children ? d : d.parent).data.name))
+		.attr('d', arc);
+
+	newSlice.append('path')
+		.attr('class', 'hidden-arc')
+		.attr('id', (_, i) => `hiddenArc${i}`)
+		.attr('d', middleArcLine);
+
+    const text = newSlice.append('text')
+		.attr('display', d => textFits(d) ? null : 'none');
+
+	// Add white contour
+	text.append('textPath')
+		.attr('startOffset', '50%')
+		.attr('xlink:href', (_, i) => `#hiddenArc${i}`)
+		.text(d => d.data.name)
+		.style('fill', 'none')
+		.style('stroke', '#fff')
+		.style('stroke-width', 2)
+		.style('stroke-linejoin', 'round');
+
+	text.append('textPath')
+		.attr('startOffset', '50%')
+		.attr('xlink:href', (_, i) => `#hiddenArc${i}`)
+		.text(d => d.data.name);
+});
+
+        function focusOn(d = { x0: 0, x1: 1, y0: 0, y1: 1 }) {
+            // Reset to top-level if no data point specified
+
+            const transition = svg.transition()
+                .duration(550)
+                .tween('scale', () => {
+                    const xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+                        yd = d3.interpolate(y.domain(), [d.y0, 1]);
+                    return t => { x.domain(xd(t)); y.domain(yd(t)); };
+                });
+
+            transition.selectAll('path.main-arc')
+                .attrTween('d', d => () => arc(d));
+
+            transition.selectAll('path.hidden-arc')
+                .attrTween('d', d => () => middleArcLine(d));
+
+            transition.selectAll('text')
+                .attrTween('display', d => () => textFits(d) ? null : 'none');
+
+            moveStackToFront(d);
+
+            //
+
+            function moveStackToFront(elD) {
+                svg.selectAll('.slice').filter(d => d === elD)
+                    .each(function(d) {
+                        this.parentNode.appendChild(this);
+                        if (d.parent) { moveStackToFront(d.parent); }
+                    })
+            }
+        }
+	/**
 	var width = 350;
     var height = 350;
 	var margin = {
@@ -142,12 +294,13 @@ function sunburstGlycanTypeSubtype(dummy, data, id) {
 
     });
 
-
+*/
     /**
      * Calculate the correct distance to rotate each label based on its location in the sunburst.
      * @param {Node} d
      * @return {Number}
      */
+	/**
     function computeTextRotation(d) {
         var angle = (d.x0 + d.x1) / Math.PI * 90;
 
@@ -155,6 +308,8 @@ function sunburstGlycanTypeSubtype(dummy, data, id) {
         return (angle < 120 || angle > 270) ? angle : angle + 180;  // labels as rims
         //return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
     }
+	*/
+	
 }
 
 /**
