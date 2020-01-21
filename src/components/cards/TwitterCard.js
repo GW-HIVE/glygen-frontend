b2d443563388ff109f0839481028e4e31006373a
Tatiana <tbw32@hotmail.com>
2020-01-20 22:39:59 -0500

    TwitterCard and it's dependencies

--- src/components/cards/TwitterCard.js
+++ src/components/cards/TwitterCard.js
@@ -6,15 +6,12 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-// import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 const useStyles = makeStyles(theme => ({
 	cardAction: {
-		display: 'inline-flex'
-	},
-	card: {
-		display: 'flex',
-		maxWidth: 345
+		display: 'inline-flex',
+		backgroundColor: 'transperent'
 	},
 	cardTitle: {
 		textAlign: 'center'
@@ -26,13 +23,13 @@ const useStyles = makeStyles(theme => ({
 
 export default function TwitterCard(props) {
 	const classes = useStyles();
-	const { post } = props;
+	// const { post } = props;
 
 	return (
 		<Grid item xs={12} sm={6} md={12}>
-			<Card className={classes.card}>
-				<CardActionArea className={classes.cardAction} component='a' href='#'>
-					{/* <Card className={classes.card}> */}
+			{/* <Card className={classes.card}> */}
+			<CardActionArea className={classes.cardAction} component='a' href='#'>
+				<Card className='card'>
 					<div className={classes.cardDetails}>
 						<CardContent>
 							<Typography
@@ -40,13 +37,30 @@ export default function TwitterCard(props) {
 								variant='h5'
 								component='h2'
 								className={classes.cardTitle}>
-								{post.title}
+								News
+								{/* {post.title} */}
 							</Typography>
+							<section className='twitterContainer'>
+								<div className='twitter-embed'>
+									<TwitterTimelineEmbed
+										sourceType='profile'
+										screenName='gly_gen'
+										options={{
+											// tweetLimit: '3',
+											width: '100%',
+											height: 500
+										}}
+										// theme='dark'
+										noHeader='true'
+										noBorders='true'
+										noFooter='true'></TwitterTimelineEmbed>
+								</div>
+							</section>
 						</CardContent>
 					</div>
-					{/* </Card> */}
-				</CardActionArea>
-			</Card>
+				</Card>
+			</CardActionArea>
+			{/* </Card> */}
 		</Grid>
 	);
 }
