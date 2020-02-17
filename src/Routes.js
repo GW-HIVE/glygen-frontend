b9baeb73ea38051609c0d77a0f1ba77abb3b84e5
Tatiana <tbw32@hotmail.com>
2020-02-17 13:13:00 -0500

    How to cite page

--- src/Routes.js
+++ src/Routes.js
@@ -3,15 +3,19 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import GlycanList from "./pages/GlycanList";
 import Resources from "./pages/Resources";
-import ProteinList from "./pages/ProteinList";
+import GlycanDetail from "./pages/GlycanDetail";
+// import ProteinList from "./pages/ProteinList";
 import GlycanListEditColumns from "./pages/GlycanListEditColumns";
+import HowToCite from "./pages/HowToCite";
 
 const Routes = props => (
   <Switch>
     <Route path="/glycan-list/:id/edit" component={GlycanListEditColumns} />
     <Route path="/glycan-list/:id" component={GlycanList} />
-    <Route path="/protein-list" component={ProteinList} />
+    {/* <Route path="/protein-list" component={ProteinList} /> */}
+    <Route path="/glycan-detail" component={GlycanDetail} />
     <Route path="/resources" component={Resources} />
+    <Route path="/how-to-cite" component={HowToCite} />
 
     {/* Keep path='/' at the bottom */}
     <Route path="/" component={Home} />
