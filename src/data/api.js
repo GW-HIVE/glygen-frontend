5203e7f9111d6a127081b3d8936fe76c38802652
Sujeet Kulkarni <sujeet27kulk@gmail.com>
2020-04-01 12:06:53 -0400

    Adding getTstJson function.

--- src/data/api.js
+++ src/data/api.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
 export const ROOT_API_URL = "https://api.glygen.org";
+export const ROOT_TST_API_URL = "https://api.tst.glygen.org/";
+
 
 export const getJson = (url, headers = {}) => {
   return axios.get(ROOT_API_URL + url, {
+    headers
+  });
+};
+
+export const getTstJson = (url, headers = {}) => {
+  return axios.get(ROOT_TST_API_URL + url, {
     headers
   });
 };
