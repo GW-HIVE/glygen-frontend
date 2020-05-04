a73fccda8315cd593fd74f254e1b94ebd231d083
Sujeet Kulkarni <sujeet27kulk@gmail.com>
2020-05-04 17:20:44 -0400

    BASELINE to BASENAME change.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import {GLYGEN_BASELINE} from "./envVariables";
+import {GLYGEN_BASENAME} from "./envVariables";
 
-ReactDOM.render(<Router basename={GLYGEN_BASELINE}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router basename={GLYGEN_BASENAME}><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
