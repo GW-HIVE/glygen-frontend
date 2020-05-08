f51ccd2d4c94cea9173b50c87c6b4df1a4fd6fba
RupaliM <rupali.mahadik88@gmail.com>
2020-05-07 23:17:37 -0400

    react copy clipboard

--- src/Routes.js
+++ src/Routes.js
@@ -1,43 +1,59 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import GlycanList from './pages/GlycanList';
-import Resources from './pages/Resources';
-import GlycanDetail from './pages/GlycanDetail';
-import GlycanListEditColumns from './pages/GlycanListEditColumns';
-import GlycanSearch from './pages/GlycanSearch';
-import HowToCite from './pages/HowToCite';
-import ContactUs from './pages/ContactUs';
-import Disclaimer from './pages/Disclaimer';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import License from './pages/License';
-import PrivacySettings from './pages/PrivacySettings';
-import Feedback from './pages/Feedback';
-import TryMeCard from './components/cards/TryMeCard';
-import routeConstants from './data/json/routeConstants.json';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import GlycanList from "./pages/GlycanList";
+import ProteinList from "./pages/ProteinList";
+import Resources from "./pages/Resources";
+import GlycanDetail from "./pages/GlycanDetail";
+import ProteinDetail from "./pages/ProteinDetail";
+import GlycanListEditColumns from "./pages/GlycanListEditColumns";
+import GlycanSearch from "./pages/GlycanSearch";
+import HowToCite from "./pages/HowToCite";
+import ContactUs from "./pages/ContactUs";
+import Disclaimer from "./pages/Disclaimer";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import License from "./pages/License";
+import PrivacySettings from "./pages/PrivacySettings";
+import Feedback from "./pages/Feedback";
+import TryMeCard from "./components/cards/TryMeCard";
+import routeConstants from "./data/json/routeConstants.json";
 
 const Routes = props => (
-	<Switch> 
-		<Route path={`${routeConstants.glycanListEdit}:id`} component={GlycanListEditColumns} />
-		<Route path={`${routeConstants.glycanList}:id`} component={GlycanList} />
-		<Route path={`${routeConstants.glycanSearch}:id`} component={GlycanSearch} />
-		<Route path={routeConstants.glycanSearch} component={GlycanSearch} />
-		{/* // <Route path={`${RouteConstant.proteinList}:id`} component={ProteinList} /> */}
-		<Route path={`${routeConstants.glycanDetail}:id`} component={GlycanDetail} />
-		<Route path={routeConstants.glycanDetail} component={GlycanDetail} />
-		<Route path={routeConstants.resources} component={Resources} />
-		<Route path={routeConstants.howToCite} component={HowToCite} />
-		<Route path={routeConstants.contactUs} component={ContactUs} />
-		<Route path={routeConstants.disclaimer} component={Disclaimer} />
-		<Route path={routeConstants.privacyPolicy} component={PrivacyPolicy} />
-		<Route path={routeConstants.license} component={License} />
-		<Route path={routeConstants.privacySettings} component={PrivacySettings} />
-		<Route path={routeConstants.feedback} component={Feedback} />
-		<Route path={routeConstants.tryMe} component={TryMeCard} />
-		{/* Keep path='/' at the bottom */}
-		<Route path={routeConstants.home} component={Home} />
-		<Route path={routeConstants.default} component={Home} />
-	</Switch>
+  <Switch>
+    <Route
+      path={`${routeConstants.glycanListEdit}:id`}
+      component={GlycanListEditColumns}
+    />
+    <Route path={`${routeConstants.glycanList}:id`} component={GlycanList} />
+    <Route path={`${routeConstants.proteinList}:id`} component={ProteinList} />
+    <Route
+      path={`${routeConstants.glycanSearch}:id`}
+      component={GlycanSearch}
+    />
+    <Route path={routeConstants.glycanSearch} component={GlycanSearch} />{" "}
+    <Route
+      path={`${routeConstants.glycanDetail}:id`}
+      component={GlycanDetail}
+    />
+    <Route path={routeConstants.glycanDetail} component={GlycanDetail} />
+    <Route
+      path={`${routeConstants.proteinDetail}:id`}
+      component={ProteinDetail}
+    />
+    <Route path={routeConstants.proteinDetail} component={ProteinDetail} />
+    <Route path={routeConstants.resources} component={Resources} />
+    <Route path={routeConstants.howToCite} component={HowToCite} />
+    <Route path={routeConstants.contactUs} component={ContactUs} />
+    <Route path={routeConstants.disclaimer} component={Disclaimer} />
+    <Route path={routeConstants.privacyPolicy} component={PrivacyPolicy} />
+    <Route path={routeConstants.license} component={License} />
+    <Route path={routeConstants.privacySettings} component={PrivacySettings} />
+    <Route path={routeConstants.feedback} component={Feedback} />
+    <Route path={routeConstants.tryMe} component={TryMeCard} />
+    {/* Keep path='/' at the bottom */}
+    <Route path={routeConstants.home} component={Home} />
+    <Route path={routeConstants.default} component={Home} />
+  </Switch>
 );
 
 export default Routes;
--- src/pages/ProteinList.js
+++ src/pages/ProteinList.js
@@ -1,52 +1,108 @@
 import React, { useState, useEffect } from "react";
-
+import Helmet from "react-helmet";
+import { getTitle } from "../utils/head";
+import { useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
-import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
+import { getProteinList } from "../data";
+import { PROTEIN_COLUMNS, getUserSelectedColumns } from "../data/protein";
+// import GlycanQuerySummary from "../components/GlycanQuerySummary";
+import PaginatedTable from "../components/PaginatedTable";
+import Container from "@material-ui/core/Container";
+// import DownloadButton from "../components/DownloadButton";
+import FeedbackWidget from "../components/FeedbackWidget";
+import stringConstants from "../data/json/stringConstants.json";
+import ReactHtmlParser from "react-html-parser";
+import routeConstants from "../data/json/routeConstants";
 
-import { getProtienList } from "../data";
-// import SelectableTable from "../components/SelectableTable";
+const proteinStrings = stringConstants.protein.common;
 
 const ProteinList = props => {
+  let { id } = useParams();
+
   const [data, setData] = useState([]);
+  const [query, setQuery] = useState([]);
+  const [pagination, setPagination] = useState([]);
+  const [selectedColumns, setSelectedColumns] = useState(PROTEIN_COLUMNS);
+  const [page, setPage] = useState(1);
+  const [sizePerPage, setSizePerPage] = useState(20);
+  const [totalSize, setTotalSize] = useState();
 
   useEffect(() => {
-    getProtienList("96729ced790bc5e300144422dd900bf3").then(({ data }) => {
-      // place to change values before rendering
-
+    getProteinList(id).then(({ data }) => {
       setData(data.results);
-      // setQuery(data.query);
-      // setPagination(data.pagination);
+
+      setPagination(data.pagination);
+      const currentPage = (data.pagination.offset - 1) / sizePerPage + 1;
+      setPage(currentPage);
+      setTotalSize(data.pagination.total_length);
     });
   }, []);
 
-  //   {
-  //     "uniprot_canonical_ac": "P14210-1",
-  //     "mass": 83134.0,
-  //     "protein_name_short": "",
-  //     "refseq_ac": "NP_000592.3",
-  //     "refseq_name": "hepatocyte growth factor isoform 1 preproprotein",
-  //     "organism": "Homo sapiens",
-  //     "gene_name": "HGF",
-  //     "protein_name_long": "Hepatocyte growth factor"
-  //   }
-  const columnDefinition = [
-    {
-      dataField: "uniprot_canonical_ac",
-      text: "Protein ID",
-      sort: true,
-      selected: true
-    }
-    // { dataField: 'mass', text: 'Mass', sort: true, selected:true},
-    // { dataField: 'iupac', text: 'iupac',sort: true, selected:true},
-    // { dataField: 'glycoct', text: 'glycoct',sort: true},
-    // { dataField: 'mass_pme', text: 'mass_pme',sort: true}
-  ];
+  const handleTableChange = (
+    type,
+    { page, sizePerPage, sortField, sortOrder }
+  ) => {
+    setPage(page);
+    setSizePerPage(sizePerPage);
+
+    getProteinList(
+      id,
+      (page - 1) * sizePerPage + 1,
+      sizePerPage,
+      sortField,
+      sortOrder
+    ).then(({ data }) => {
+      // place to change values before rendering
+
+      setData(data.results);
+
+      setPagination(data.pagination);
+
+      setTotalSize(data.pagination.total_length);
+    });
+  };
+
+  function rowStyleFormat(row, rowIdx) {
+    return { backgroundColor: rowIdx % 2 === 0 ? "red" : "blue" };
+  }
 
   return (
     <>
-      {/* <SelectableTable columns={columnDefinition} data={data} keyField='uniprot_canonical_ac'/> */}
-      {/* <ListTable selectedColumns={columns.selected} data={data}  />  */}
+      <Helmet>
+        {getTitle("proteinList")}
+        {getTitle("proteinList")}
+      </Helmet>
+
+      <FeedbackWidget />
+      <Container maxWidth="xl" className="ggContainer">
+        <section className="content-box-md">
+          {/* <section style={{ paddingTop: "20px" }}> */}
+          {/* <GlycanQuerySummary
+            data={query}
+            onModifySearch={handleModifySearch}
+          /> */}
+        </section>
+        <section>
+          {/* <DownloadButton
+            types={[
+              { display: "Glycan data (*.csv)", type: "csv", data: "glycan" },
+              { display: "Glycan data (*.json)", type: "json", data: "glycan" }
+            ]}
+            dataId={id}
+          /> */}
+          {selectedColumns && selectedColumns.length !== 0 && (
+            <PaginatedTable
+              trStyle={rowStyleFormat}
+              data={data}
+              columns={selectedColumns}
+              page={page}
+              sizePerPage={sizePerPage}
+              totalSize={totalSize}
+              onTableChange={handleTableChange}
+            />
+          )}
+        </section>
+      </Container>
     </>
   );
 };
