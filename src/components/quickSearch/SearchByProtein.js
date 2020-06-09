b51e95ca9d403e65bba6d0494cba467ef2a915be
Tatiana <tbw32@hotmail.com>
2020-06-08 20:40:01 -0400

    Quick search page

---
+++ src/components/quickSearch/SearchByProtein.js
@@ -0,0 +1,90 @@
+import React from "react";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import quickSearchData from "../../data/json/questions.json";
+
+const SearchByProtein = (props) => {
+	return (
+		<>
+			<div id={props.id}>
+				<section className="content-box-sm" style={{ margin: "0 auto" }}>
+					<div className="quick-search-heading">
+						<h4>Search by Protein</h4>
+					</div>
+					<div className="quick-search">
+						<ExpansionPanel>
+							<ExpansionPanelSummary
+								expandIcon={<ExpandMoreIcon className="gg-blue-color" />}
+								aria-controls="panel1bh-content"
+								id="panel1bh-header">
+								<Typography className="gg-blue-color">
+									{quickSearchData.quickSearch.searchByProtein.question_1.text}
+								</Typography>
+							</ExpansionPanelSummary>
+							<ExpansionPanelDetails>
+								<Typography>
+									Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+									feugiat. Aliquam eget maximus est, id dignissim quam.
+								</Typography>
+							</ExpansionPanelDetails>
+						</ExpansionPanel>
+						<ExpansionPanel>
+							<ExpansionPanelSummary
+								expandIcon={<ExpandMoreIcon className="gg-blue-color" />}
+								aria-controls="panel2bh-content"
+								id="panel2bh-header">
+								<Typography className="gg-blue-color">
+									{quickSearchData.quickSearch.searchByProtein.question_2.text}
+								</Typography>
+							</ExpansionPanelSummary>
+							<ExpansionPanelDetails>
+								<Typography>
+									Donec placerat, lectus sed mattis semper, neque lectus feugiat
+									lectus, varius pulvinar diam eros in elit. Pellentesque
+									convallis laoreet laoreet.
+								</Typography>
+							</ExpansionPanelDetails>
+						</ExpansionPanel>
+						<ExpansionPanel>
+							<ExpansionPanelSummary
+								expandIcon={<ExpandMoreIcon className="gg-blue-color" />}
+								aria-controls="panel3bh-content"
+								id="panel3bh-header">
+								<Typography className="gg-blue-color">
+									{quickSearchData.quickSearch.searchByProtein.question_3.text}
+								</Typography>
+							</ExpansionPanelSummary>
+							<ExpansionPanelDetails>
+								<Typography>
+									Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+									Integer sit amet egestas eros, vitae egestas augue. Duis vel
+									est augue.
+								</Typography>
+							</ExpansionPanelDetails>
+						</ExpansionPanel>
+						<ExpansionPanel>
+							<ExpansionPanelSummary
+								expandIcon={<ExpandMoreIcon className="gg-blue-color" />}
+								aria-controls="panel1bh-content"
+								id="panel1bh-header">
+								<Typography className="gg-blue-color">
+									{quickSearchData.quickSearch.searchByProtein.question_4.text}
+								</Typography>
+							</ExpansionPanelSummary>
+							<ExpansionPanelDetails>
+								<Typography>
+									Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+									feugiat. Aliquam eget maximus est, id dignissim quam.
+								</Typography>
+							</ExpansionPanelDetails>
+						</ExpansionPanel>
+					</div>
+				</section>
+			</div>
+		</>
+	);
+};
+export default SearchByProtein;
---
+++ src/pages/QuickSearch.js
@@ -0,0 +1,116 @@
+import React, { useEffect } from "react";
+import Helmet from "react-helmet";
+import { getTitle, getMeta } from "../utils/head";
+import Container from "@material-ui/core/Container";
+import VerticalHeading from "../components/headings/VerticalHeading";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import { Row, Col } from "react-bootstrap";
+import Sidebar from "../components/navigation/Sidebar";
+import { logActivity } from "../data/logging";
+import SearchByGlycan from "../components/quickSearch/SearchByGlycan";
+import SearchByProtein from "../components/quickSearch/SearchByProtein";
+import SearchBySpecies from "../components/quickSearch/SearchBySpecies";
+import SearchByDisease from "../components/quickSearch/SearchByDisease";
+
+const QuickSearch = (props) => {
+	const vertHeadQuickSearch = {
+		h5VerticalText: "Searches",
+		h2textTop: "Perform",
+		h2textBottom: "A",
+		h2textBottomStrongAfter: "Quick Search",
+	};
+
+	const items = [
+		{ label: "Glycan", id: "glycan" },
+		{ label: "Protein", id: "protein" },
+		{ label: "Species", id: "species" },
+		{ label: "Disease", id: "disease" },
+	];
+	useEffect(() => {
+		logActivity();
+	}, []);
+
+	return (
+		<>
+			<Helmet>
+				{getTitle("quickSearch")}
+				{getMeta("quickSearch")}
+			</Helmet>
+
+			<div id="top-heading"></div>
+			<Row className="gg-baseline5">
+				<Col sm={12} md={12} lg={12} xl={3} className="sidebar-col">
+					<Sidebar items={items} />
+				</Col>
+				<Col sm={12} md={12} lg={12} xl={9} className="sidebar-page">
+					<Container maxWidth="md">
+						<VerticalHeading
+							post={vertHeadQuickSearch}
+							style={{ margin: "0 auto" }}
+						/>
+						<SearchByGlycan id="glycan" />
+						<SearchByProtein id="protein" />
+						<SearchBySpecies id="species" />
+						<SearchByDisease id="disease" />
+					</Container>
+				</Col>
+			</Row>
+			{/* </Container> */}
+		</>
+	);
+};
+export default QuickSearch;
+
+// /* eslint-disable jsx-a11y/anchor-is-valid */
+// import React, { useEffect } from "react";
+// import Helmet from "react-helmet";
+// import { getTitle, getMeta } from "../utils/head";
+// import Sidebar from "../components/navigation/Sidebar";
+// import { logActivity } from "../data/logging";
+// import HorHeadQuickSearch from "../components/headings/HorizontalHeading";
+
+// import { Col, Row } from "react-bootstrap";
+
+// import FeedbackWidget from "../components/FeedbackWidget";
+
+// const QuickSearch = (props) => {
+// 	const items = [
+// 		{ label: "Glycan", id: "glycan" },
+// 		{ label: "Protein", id: "protein" },
+// 		{ label: "Species", id: "species" },
+// 		{ label: "Disease", id: "disease" },
+// 	];
+// 	useEffect(() => {
+// 		logActivity();
+// 	}, []);
+
+// 	const horHeadQuickSearch = {
+// 		h5VerticalText: "ADVANCED SEARCH",
+// 		h2textTopStrongBefore: "How To",
+// 		h2textTop: "Use",
+// 		h2textTopStrongAfter: "Advanced",
+// 		h2textTop2: "Glycan Search",
+// 	};
+// 	return (
+// 		<>
+// 			<Helmet>
+// 				{getTitle("quickSearch")}
+// 				{getMeta("quickSearch")}
+// 			</Helmet>
+// 			<FeedbackWidget />
+
+// 			<Row className="gg-baseline">
+// 				<Col sm={12} md={12} lg={12} xl={3} className="sidebar-col">
+// 					<Sidebar items={items} />
+// 				</Col>
+// 				<Col sm={12} md={12} lg={12} xl={9} className="sidebar-page">
+// 					<HorHeadQuickSearch post={horHeadQuickSearch} />
+// 					<PanelHowToCite id="howToCite" data={howToCiteData.howToCite} />
+// 				</Col>
+// 			</Row>
+// 		</>
+// 	);
+// };
+
+// export default QuickSearch;
