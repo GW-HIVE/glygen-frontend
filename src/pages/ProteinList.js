538a2b9db2b806b4b81843466ee55feb91b0805e
RupaliM <rupali.mahadik88@gmail.com>
2020-07-06 17:33:17 -0400

    syntax error fixed

--- src/pages/ProteinList.js
+++ src/pages/ProteinList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useReducer } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import Helmet from "react-helmet";
 import { getTitle, getMeta } from "../utils/head";
 import { useParams } from "react-router-dom";
@@ -14,9 +14,9 @@ import stringConstants from "../data/json/stringConstants.json";
 import ReactHtmlParser from "react-html-parser";
 import routeConstants from "../data/json/routeConstants";
 import { logActivity } from "../data/logging";
-import PageLoader from '../components/load/PageLoader';
-import DialogAlert from '../components/alert/DialogAlert';
-import {axiosError} from '../data/axiosError';
+import PageLoader from "../components/load/PageLoader";
+import DialogAlert from "../components/alert/DialogAlert";
+import { axiosError } from "../data/axiosError";
 const proteinStrings = stringConstants.protein.common;
 
 const ProteinList = props => {
@@ -31,33 +31,35 @@ const ProteinList = props => {
   const [sizePerPage, setSizePerPage] = useState(20);
   const [totalSize, setTotalSize] = useState();
   const [pageLoading, setPageLoading] = useState(true);
-	const [alertDialogInput, setAlertDialogInput] = useReducer(
-		(state, newState) => ({ ...state, ...newState }),
-		{show: false, id: ""}
+  const [alertDialogInput, setAlertDialogInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { show: false, id: "" }
   );
 
   useEffect(() => {
     setPageLoading(true);
-		logActivity("user", id);
-    getProteinList(id).then(({ data }) => {
-      if (data.error_code) {
-        let message = "list api call";
-        logActivity("user", id, "No results. " + message);
-        setPageLoading(false);
-      } else {
-      setData(data.results);
-      setQuery(data.query);
+    logActivity("user", id);
+    getProteinList(id)
+      .then(({ data }) => {
+        if (data.error_code) {
+          let message = "list api call";
+          logActivity("user", id, "No results. " + message);
+          setPageLoading(false);
+        } else {
+          setData(data.results);
+          setQuery(data.query);
 
-      setPagination(data.pagination);
-      const currentPage = (data.pagination.offset - 1) / sizePerPage + 1;
-      setPage(currentPage);
-      setTotalSize(data.pagination.total_length);
-      setPageLoading(false);
-    })
-    .catch(function (error) {
-			let message = "list api call";
-			axiosError(error, id, message, setPageLoading, setAlertDialogInput);
-    });
+          setPagination(data.pagination);
+          const currentPage = (data.pagination.offset - 1) / sizePerPage + 1;
+          setPage(currentPage);
+          setTotalSize(data.pagination.total_length);
+          setPageLoading(false);
+        }
+      })
+      .catch(function(error) {
+        let message = "list api call";
+        axiosError(error, id, message, setPageLoading, setAlertDialogInput);
+      });
   }, []);
 
   const handleTableChange = (
@@ -76,21 +78,27 @@ const ProteinList = props => {
     ).then(({ data }) => {
       // place to change values before rendering
       if (!data.error_code) {
-      setData(data.results);
+        setData(data.results);
 
-      setPagination(data.pagination);
+        setPagination(data.pagination);
 
-      setTotalSize(data.pagination.total_length);
-    }
+        setTotalSize(data.pagination.total_length);
+      }
     });
   };
   const handleModifySearch = () => {
     if (searchId === "gs")
       props.history.push(routeConstants.globalSearchResult + query.term);
     else if (quickSearch[searchId] !== undefined)
-      props.history.push(routeConstants.quickSearch + id + "/" + quickSearch[searchId].id + "#" + quickSearch[searchId].id);
-    else 
-      props.history.push(routeConstants.proteinSearch + id);
+      props.history.push(
+        routeConstants.quickSearch +
+          id +
+          "/" +
+          quickSearch[searchId].id +
+          "#" +
+          quickSearch[searchId].id
+      );
+    else props.history.push(routeConstants.proteinSearch + id);
   };
 
   function rowStyleFormat(row, rowIdx) {
@@ -106,13 +114,13 @@ const ProteinList = props => {
 
       <FeedbackWidget />
       <Container maxWidth="xl" className="gg-container">
-      <PageLoader pageLoading={pageLoading} />
-					<DialogAlert
-						alertInput={alertDialogInput}
-						setOpen={(input) => {
-							setAlertDialogInput({"show": input})
-						}}
-					/>
+        <PageLoader pageLoading={pageLoading} />
+        <DialogAlert
+          alertInput={alertDialogInput}
+          setOpen={input => {
+            setAlertDialogInput({ show: input });
+          }}
+        />
         <section className="content-box-md">
           {query && (
             <ProteinQuerySummary
