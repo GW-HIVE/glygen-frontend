9d384120377569bf23f9a075f83d0d2dd388e04c
RupaliM <rupali.mahadik88@gmail.com>
2020-07-06 12:51:15 -0400

    logging activity

--- src/pages/GlycanList.js
+++ src/pages/GlycanList.js
@@ -13,7 +13,10 @@ import FeedbackWidget from "../components/FeedbackWidget";
 import stringConstants from "../data/json/stringConstants.json";
 import ReactHtmlParser from "react-html-parser";
 import routeConstants from "../data/json/routeConstants";
-
+import { logActivity } from "../data/logging";
+import PageLoader from "../components/load/PageLoader";
+import DialogAlert from "../components/alert/DialogAlert";
+import { axiosError } from "../data/axiosError";
 const GlycanList = props => {
   let { id } = useParams();
   let { searchId } = useParams();
@@ -26,6 +29,11 @@ const GlycanList = props => {
   const [page, setPage] = useState(1);
   const [sizePerPage, setSizePerPage] = useState(20);
   const [totalSize, setTotalSize] = useState();
+  const [pageLoading, setPageLoading] = useState(true);
+  const [alertDialogInput, setAlertDialogInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { show: false, id: "" }
+  );
 
   const fixResidueToShortNames = query => {
     const residueMap = stringConstants.glycan.common.composition;
@@ -53,22 +61,35 @@ const GlycanList = props => {
   };
 
   useEffect(() => {
+    setPageLoading(true);
+    logActivity("user", id);
     // const selected = getUserSelectedColumns();
     // const userSelectedColumn = GLYCAN_COLUMNS.filter(column =>
     //   selected.includes(column.dataField)
     // );
     // setSelectedColumns(userSelectedColumn);
 
-    getGlycanList(id).then(({ data }) => {
-      setData(data.results);
-      setQuery(fixResidueToShortNames(data.query));
-      setPagination(data.pagination);
-      const currentPage = (data.pagination.offset - 1) / sizePerPage + 1;
-      setPage(currentPage);
-      //   setSizePerPage()
-      setTotalSize(data.pagination.total_length);
-    });
-    // eslint-disable-next-line
+    getGlycanList(id)
+      .then(({ data }) => {
+        if (data.error_code) {
+          let message = "list api call";
+          logActivity("user", id, "No results. " + message);
+          setPageLoading(false);
+        } else {
+          setData(data.results);
+          setQuery(fixResidueToShortNames(data.query));
+          setPagination(data.pagination);
+          const currentPage = (data.pagination.offset - 1) / sizePerPage + 1;
+          setPage(currentPage);
+          //   setSizePerPage()
+          setTotalSize(data.pagination.total_length);
+          setPageLoading(false);
+        }
+      })
+      .catch(function(error) {
+        let message = "list api call";
+        axiosError(error, id, message, setPageLoading, setAlertDialogInput);
+      });
   }, []);
 
   const handleTableChange = (
@@ -103,9 +124,15 @@ const GlycanList = props => {
     if (searchId === "gs")
       props.history.push(routeConstants.globalSearchResult + query.term);
     else if (quickSearch[searchId] !== undefined)
-      props.history.push(routeConstants.quickSearch + id + "/" +  quickSearch[searchId].id + "#" + quickSearch[searchId].id);
-    else 
-      props.history.push(routeConstants.glycanSearch + id);
+      props.history.push(
+        routeConstants.quickSearch +
+          id +
+          "/" +
+          quickSearch[searchId].id +
+          "#" +
+          quickSearch[searchId].id
+      );
+    else props.history.push(routeConstants.glycanSearch + id);
   };
 
   return (
@@ -117,8 +144,14 @@ const GlycanList = props => {
 
       <FeedbackWidget />
       <Container maxWidth="xl" className="gg-container">
+        <PageLoader pageLoading={pageLoading} />
+        <DialogAlert
+          alertInput={alertDialogInput}
+          setOpen={input => {
+            setAlertDialogInput({ show: input });
+          }}
+        />
         <section className="content-box-md">
-          {/* <section style={{ paddingTop: "20px" }}> */}
           <GlycanQuerySummary
             data={query}
             question={quickSearch[searchId]}
