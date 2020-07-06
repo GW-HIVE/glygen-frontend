14485e5996215c7b088ad5009aa4038bb1b037dd
RupaliM <rupali.mahadik88@gmail.com>
2020-07-06 13:14:57 -0400

    logging activity for protein list

--- src/pages/ProteinList.js
+++ src/pages/ProteinList.js
@@ -13,15 +13,16 @@ import FeedbackWidget from "../components/FeedbackWidget";
 import stringConstants from "../data/json/stringConstants.json";
 import ReactHtmlParser from "react-html-parser";
 import routeConstants from "../data/json/routeConstants";
-
-
+import { logActivity } from "../data/logging";
+import PageLoader from '../components/load/PageLoader';
+import DialogAlert from '../components/alert/DialogAlert';
+import {axiosError} from '../data/axiosError';
 const proteinStrings = stringConstants.protein.common;
 
 const ProteinList = props => {
   let { id } = useParams();
   let { searchId } = useParams();
   let quickSearch = stringConstants.quick_search;
-
   const [data, setData] = useState([]);
   const [query, setQuery] = useState([]);
   const [pagination, setPagination] = useState([]);
@@ -29,9 +30,21 @@ const ProteinList = props => {
   const [page, setPage] = useState(1);
   const [sizePerPage, setSizePerPage] = useState(20);
   const [totalSize, setTotalSize] = useState();
+  const [pageLoading, setPageLoading] = useState(true);
+	const [alertDialogInput, setAlertDialogInput] = useReducer(
+		(state, newState) => ({ ...state, ...newState }),
+		{show: false, id: ""}
+  );
 
   useEffect(() => {
+    setPageLoading(true);
+		logActivity("user", id);
     getProteinList(id).then(({ data }) => {
+      if (data.error_code) {
+        let message = "list api call";
+        logActivity("user", id, "No results. " + message);
+        setPageLoading(false);
+      } else {
       setData(data.results);
       setQuery(data.query);
 
@@ -39,6 +52,11 @@ const ProteinList = props => {
       const currentPage = (data.pagination.offset - 1) / sizePerPage + 1;
       setPage(currentPage);
       setTotalSize(data.pagination.total_length);
+      setPageLoading(false);
+    })
+    .catch(function (error) {
+			let message = "list api call";
+			axiosError(error, id, message, setPageLoading, setAlertDialogInput);
     });
   }, []);
 
@@ -57,12 +75,13 @@ const ProteinList = props => {
       sortOrder
     ).then(({ data }) => {
       // place to change values before rendering
-
+      if (!data.error_code) {
       setData(data.results);
 
       setPagination(data.pagination);
 
       setTotalSize(data.pagination.total_length);
+    }
     });
   };
   const handleModifySearch = () => {
@@ -87,6 +106,13 @@ const ProteinList = props => {
 
       <FeedbackWidget />
       <Container maxWidth="xl" className="gg-container">
+      <PageLoader pageLoading={pageLoading} />
+					<DialogAlert
+						alertInput={alertDialogInput}
+						setOpen={(input) => {
+							setAlertDialogInput({"show": input})
+						}}
+					/>
         <section className="content-box-md">
           {query && (
             <ProteinQuerySummary
