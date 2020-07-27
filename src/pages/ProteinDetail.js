55cc5cbba9dd1218cba202781bbbf10f314c2a94
RupaliM <rupali.mahadik88@gmail.com>
2020-07-27 09:45:25 -0400

    Revert "sequences"
    This reverts commit 4381eab65e92580765a95de2bc4750387f7bb280.

--- src/pages/ProteinDetail.js
+++ src/pages/ProteinDetail.js
@@ -265,7 +265,9 @@ const ProteinDetail = (props) => {
 	} 
 	= detailData;
 
-	
+	const speciesid = taxid;
+	const speciesUrl =
+		"https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=" + speciesid;
 	const speciesEvidence = groupSpeciesEvidences(species);
 	const glycoSylationColumns = [
 		{
@@ -822,8 +824,7 @@ const ProteinDetail = (props) => {
 																{proteinStrings.sequence_length.name}:{" "}
 															</strong>
 															<Link
-																href={`https://www.uniprot.org/uniprot/${uniprot.uniprot_canonical_ac}/#sequences`}
-																
+																href="https://www.uniprot.org/uniprot/#sequence"
 																target="_blank"
 																rel="noopener noreferrer">
 																{uniprot.length}
