b36982408aa0cb3412716f523ee09b7e38858cc1
Tatiana <tbw32@hotmail.com>
2020-07-28 12:08:22 -0400

    MainFeaturedCard two proposals

--- src/components/cards/MainFeaturedCard.js
+++ src/components/cards/MainFeaturedCard.js
@@ -136,3 +136,274 @@ export default function MainFeaturedCard(props) {
 MainFeaturedCard.propTypes = {
 	post: PropTypes.object,
 };
+
+// 2 Proposals
+
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Paper from "@material-ui/core/Paper";
+// import Typography from "@material-ui/core/Typography";
+// import Grid from "@material-ui/core/Grid";
+// import { Link } from "react-router-dom";
+// import HourglassEmptyIcon from "@material-ui/icons/HourglassEmpty";
+// import PersonPinCircleOutlinedIcon from "@material-ui/icons/PersonPinCircleOutlined";
+// import quoteIcon from "../../images/icons/quote-open-outline-white.svg";
+// import routeConstants from "../../data/json/routeConstants.json";
+
+// const useStyles = makeStyles((theme) => ({
+// 	mainFeaturedCard: {
+// 		position: "relative",
+// 		backgroundColor: theme.palette.grey[800],
+// 		color: theme.palette.common.white,
+// 		marginBottom: theme.spacing(4),
+// 		// backgroundImage: 'url(https://source.unsplash.com/random)',
+// 		backgroundSize: "cover",
+// 		backgroundRepeat: "no-repeat",
+// 		backgroundPosition: "center",
+// 	},
+// 	overlay: {
+// 		position: "absolute",
+// 		top: 0,
+// 		bottom: 0,
+// 		right: 0,
+// 		left: 0,
+// 		backgroundColor: "rgba(0,0,0,.3)",
+// 	},
+// 	mainFeaturedCardContent: {
+// 		position: "relative",
+// 		// height: "400px !important",
+// 		textAlign: "center",
+// 		padding: theme.spacing(3),
+// 		[theme.breakpoints.up("md")]: {
+// 			padding: theme.spacing(3),
+// 		},
+// 	},
+// 	linkText: {
+// 		color: "#ffffff !important",
+// 		fontWeight: "600",
+// 		"&:hover": {
+// 			color: "#57affa",
+// 		},
+// 	},
+// }));
+// export default function MainFeaturedCard(props) {
+// 	const classes = useStyles();
+// 	const { post } = props;
+
+// 	return (
+// 		<Paper
+// 			className={classes.mainFeaturedCard}
+// 			style={{ backgroundImage: `url(${post.image})` }}>
+// 			{
+// 				<img
+// 					style={{ display: "none" }}
+// 					src={post.image}
+// 					alt={post.imageText}
+// 				/>
+// 			}
+// 			<div className={classes.overlay} />
+// 			<Grid container className="gg-align-center">
+// 				<Grid item sm={12} lg={9}>
+// 					<div className={classes.mainFeaturedCardContent}>
+// 						<Typography
+// 							component="h5"
+// 							variant="h6"
+// 							color="inherit"
+// 							gutterBottom
+// 							// style={ { letterSpacing: "1px" } }
+// 						>
+// 							{post.title}
+// 						</Typography>
+// 						<Typography
+// 							component="h6"
+// 							color="inherit"
+// 							paragraph
+// 							// style={ { letterSpacing: "1px" } }
+// 						>
+// 							{post.description}
+// 						</Typography>
+// 						{/* <Typography paragraph style={{ marginBottom: "40px" }}>
+// 							<Link as={NavLink} to={post.toAbout} className={classes.linkText}>
+// 								{post.linkText}
+// 							</Link>
+// 						</Typography> */}
+// 						<Typography paragraph style={{ marginTop: "30px" }}>
+// 							<Link
+// 								to={routeConstants.howToCite}
+// 								className="gg-btn-outline-white">
+// 								<span>
+// 									<img
+// 										component="img"
+// 										style={{ paddingRight: "15px", marginTop: "-5px" }}
+// 										src={quoteIcon}
+// 										alt="quote icon"
+// 									/>
+// 								</span>
+// 								How To Cite
+// 							</Link>
+// 							<Link to={routeConstants.tryMe} className="gg-btn-outline-white">
+// 								<span>
+// 									<HourglassEmptyIcon
+// 										style={{ paddingRight: "10px", marginTop: "-5px" }}
+// 									/>
+// 								</span>
+// 								Quick Start
+// 							</Link>
+// 							<Link to={routeConstants.about} className="gg-btn-outline-white">
+// 								<span>
+// 									<PersonPinCircleOutlinedIcon
+// 										style={{ paddingRight: "10px", marginTop: "-5px" }}
+// 									/>
+// 								</span>
+// 								Our Mission
+// 							</Link>
+// 						</Typography>
+// 						<Grid item></Grid>
+// 					</div>
+// 				</Grid>
+// 			</Grid>
+// 		</Paper>
+// 	);
+// }
+
+// MainFeaturedCard.propTypes = {
+// 	post: PropTypes.object,
+// };
+
+// 07 / 27 / 2020;
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Paper from "@material-ui/core/Paper";
+// import Typography from "@material-ui/core/Typography";
+// import Grid from "@material-ui/core/Grid";
+// import HourglassEmptyIcon from "@material-ui/icons/HourglassEmpty";
+// import PersonPinCircleOutlinedIcon from "@material-ui/icons/PersonPinCircleOutlined";
+// import quoteIcon from "../../images/icons/quote-open-outline-white.svg";
+// import routeConstants from "../../data/json/routeConstants.json";
+// import { Link } from "react-router-dom";
+
+// const useStyles = makeStyles((theme) => ({
+// 	mainFeaturedCard: {
+// 		position: "relative",
+// 		backgroundColor: theme.palette.grey[800],
+// 		color: theme.palette.common.white,
+// 		marginBottom: theme.spacing(4),
+// 		// backgroundImage: 'url(https://source.unsplash.com/random)',
+// 		backgroundSize: "cover",
+// 		backgroundRepeat: "no-repeat",
+// 		backgroundPosition: "center",
+// 	},
+// 	overlay: {
+// 		position: "absolute",
+// 		top: 0,
+// 		bottom: 0,
+// 		right: 0,
+// 		left: 0,
+// 		backgroundColor: "rgba(0,0,0,.3)",
+// 	},
+// 	mainFeaturedCardContent: {
+// 		position: "relative",
+// 		// height: "400px !important",
+// 		// textAlign: "center",
+// 		// textAlign: "left",
+// 		padding: theme.spacing(3),
+// 		[theme.breakpoints.up("md")]: {
+// 			padding: theme.spacing(6),
+// 		},
+// 	},
+// 	linkText: {
+// 		color: "#ffffff !important",
+// 		fontWeight: "600",
+// 		"&:hover": {
+// 			color: "#57affa",
+// 		},
+// 	},
+// }));
+// export default function MainFeaturedCard(props) {
+// 	const classes = useStyles();
+// 	const { post } = props;
+
+// 	return (
+// 		<Paper
+// 			className={classes.mainFeaturedCard}
+// 			style={{ backgroundImage: `url(${post.image})` }}>
+// 			{
+// 				<img
+// 					style={{ display: "none" }}
+// 					src={post.image}
+// 					alt={post.imageText}
+// 				/>
+// 			}
+// 			<div className={classes.overlay} />
+// 			{/* <Grid container className="gg-align-center"> */}
+// 			<Grid container>
+// 				{/* <Grid item sm={ 12 } lg={ 8 }> */}
+// 				<Grid item sm={12} md={7} className="text-left">
+// 					<div className={classes.mainFeaturedCardContent}>
+// 						<Typography
+// 							component="h5"
+// 							variant="h6"
+// 							color="inherit"
+// 							gutterBottom>
+// 							{post.title}
+// 						</Typography>
+// 						<Typography component="h6" color="inherit" paragraph>
+// 							{post.description}
+// 						</Typography>
+// 					</div>
+// 				</Grid>
+// 				<Grid item sm={12} md={3} lg={3} className="text-right"></Grid>
+// 				<Grid item sm={12} md={2} lg={2} className="text-center">
+// 					<div className={classes.mainFeaturedCardContent}>
+// 						<Typography>
+// 							<Link
+// 								style={{ margin: "5px" }}
+// 								to={routeConstants.howToCite}
+// 								className="gg-btn-outline-white">
+// 								<span>
+// 									<img
+// 										component="img"
+// 										style={{ paddingRight: "15px", marginTop: "-5px" }}
+// 										src={quoteIcon}
+// 										alt="quote icon"
+// 									/>
+// 								</span>
+// 								How To Cite
+// 							</Link>
+
+// 							<Link
+// 								style={{ margin: "5px" }}
+// 								to={routeConstants.tryMe}
+// 								className="gg-btn-outline-white">
+// 								<span>
+// 									<HourglassEmptyIcon
+// 										style={{ paddingRight: "10px", marginTop: "-5px" }}
+// 									/>
+// 								</span>
+// 								Quick Start
+// 							</Link>
+
+// 							<Link
+// 								style={{ margin: "5px" }}
+// 								to={routeConstants.about}
+// 								className="gg-btn-outline-white">
+// 								<span>
+// 									<PersonPinCircleOutlinedIcon
+// 										style={{ paddingRight: "10px", marginTop: "-5px" }}
+// 									/>
+// 								</span>
+// 								Our Mission
+// 							</Link>
+// 						</Typography>
+// 					</div>
+// 				</Grid>
+// 			</Grid>
+// 		</Paper>
+// 	);
+// }
+
+// MainFeaturedCard.propTypes = {
+// 	post: PropTypes.object,
+// };
