03fb6608d7d6bc0732d5b35905b92df9b718302a
Tatiana <tbw32@hotmail.com>
2020-09-18 14:55:51 -0400

    Updated glygenBadge colors

--- src/components/GlygenBadge.js
+++ src/components/GlygenBadge.js
@@ -3,104 +3,107 @@ import { Button, Link } from "@material-ui/core";
 import PropTypes from "prop-types";
 
 function databasecolor(name) {
-  switch (name.toLowerCase()) {
-    case "glycomedb":
-      return "#a06868";
-    case "glytoucan":
-      return "#ff6464";
-    case "unicarbkb":
-      return "#6b7f71";
-    case "uniprotkb":
-      return "#4b8aa0";
-    case "pubmed":
-      return "#7c985d";
-    case "refseq":
-      return "#3ea2ad";
-    case "ensembl peptide":
-      return "#936caf";
-    case "ensembl transcript":
-      return "#b971a6";
-    case "ensembl gene":
-      return "#468FE0";
-    case "omim":
-      return "#8d85fa";
-    case "biomuta":
-      return "#7975af";
-    case "bgee":
-      return "#798bae";
-    case "bioxpress":
-      return "#7f989a";
-    case "mgi":
-      return "#ff8080";
-    case "hgnc":
-      return "#518a8a";
-    case "homologene":
-      return "#9a039a";
-    case "oma":
-      return "#89a15c";
-    case "mgi_homologset":
-      return "#e8a2e8";
-    case "mondo":
-      return "#be9fe1";
-    case "genomics england":
-      return "#8ac6d1";
-    case "glyconnect":
-      return "#fcba03";
-    case "mim":
-      return "#eb4034";
-    case "dbsnp":
-      return "#ebbd34";
-    case "subsumption":
-      return "#33b8ff";
-    case "topmed":
-      return "#33b4gf";
-    case "gnomad":
-      return "#33bbgf";
-  }
-  return "#9033ff";
+	switch (name.toLowerCase()) {
+		case "glycomedb":
+			return "#a06868";
+		case "glytoucan":
+			return "#ff6464";
+		case "unicarbkb":
+			return "#6b7f71";
+		case "uniprotkb":
+			return "#4b8aa0";
+		case "pubmed":
+			return "#51B0A5";
+		case "refseq":
+			return "#3ea2ad";
+		case "ensembl peptide":
+			return "#936caf";
+		case "ensembl transcript":
+			return "#b971a6";
+		case "ensembl gene":
+			return "#468FE0";
+		case "omim":
+			return "#8d85fa";
+		case "biomuta":
+			return "#7975af";
+		case "bgee":
+			return "#798bae";
+		case "bioxpress":
+			return "#7f989a";
+		case "mgi":
+			return "#ff8080";
+		case "hgnc":
+			return "#518a8a";
+		case "homologene":
+			return "#9a039a";
+		case "oma":
+			return "#7FB97A";
+		case "mgi_homologset":
+			return "#e8a2e8";
+		case "mondo":
+			return "#be9fe1";
+		case "genomics england":
+			return "#8ac6d1";
+		case "glyconnect":
+			return "#fcba03";
+		case "mim":
+			return "#BF8181";
+		case "dbsnp":
+			return "#FFC47F";
+		case "subsumption":
+			return "#33b8ff";
+		case "exac":
+			return "#ff9a76";
+		case "topmed":
+			return "#e97171";
+		case "gnomad":
+			return "#AF5A62";
+		case "matrixdb":
+			return "#64958f";
+	}
+	return "#9033ff";
 }
+// 9d65c9
+const GlygenBadge = (props) => {
+	const [showExpandList, setShowExpandList] = useState(false);
 
-const GlygenBadge = props => {
-  const [showExpandList, setShowExpandList] = useState(false);
+	const { text } = props;
 
-  const { text } = props;
-
-  return (
-    <div className="badge-div">
-      <Button
-        className="badge-button"
-        style={{
-          backgroundColor: databasecolor(text),
-          color: "white"
-        }}
-        onClick={() => setShowExpandList(!showExpandList)}
-      >
-        {text}
-        &nbsp;<span className="badge-count">
-          {props.expandList.length}
-        </span>{" "}
-        {/* <Badge variant="light">{props.expandList.length}</Badge> */}
-        {/* <Badge color="primary" badgeContent={props.expandList.length}></Badge> */}
-      </Button>
-      {showExpandList && (
-        <ul style={{ listStyle: "none", padding: "5px", margin: "0" }}>
-          {props.expandList.map((value, index) => (
-            <li key={index}>
-              <Link href={value.url} target="_blank" rel="noopener noreferrer">
-                {value.id}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+	return (
+		<div className="badge-div">
+			<Button
+				className="badge-button"
+				style={{
+					backgroundColor: databasecolor(text),
+					color: "white",
+				}}
+				onClick={() => setShowExpandList(!showExpandList)}>
+				{text}
+				&nbsp;<span className="badge-count">
+					{props.expandList.length}
+				</span>{" "}
+				{/* <Badge variant="light">{props.expandList.length}</Badge> */}
+				{/* <Badge color="primary" badgeContent={props.expandList.length}></Badge> */}
+			</Button>
+			{showExpandList && (
+				<ul style={{ listStyle: "none", padding: "5px", margin: "0" }}>
+					{props.expandList.map((value, index) => (
+						<li key={index}>
+							<Link href={value.url} target="_blank" rel="noopener noreferrer">
+								{value.id}
+							</Link>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
 
 GlygenBadge.propTypes = {
-  text: PropTypes.string,
-  expandList: PropTypes.array,
-  color: PropTypes.array
+	text: PropTypes.string,
+	expandList: PropTypes.array,
+	color: PropTypes.array,
 };
 
 export default GlygenBadge;
