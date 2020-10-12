354a6bdbfa4212b92ed3f2c54258a1c3385c2cf9
Tatiana <tbw32@hotmail.com>
2020-10-12 12:06:22 -0400

    Google analytics tracking code

--- src/App.js
+++ src/App.js
@@ -9,72 +9,72 @@ import ReactGA from "react-ga";
 import { GLYGEN_ENV } from "./envVariables.js";
 
 function initializeReactGA() {
-	if (GLYGEN_ENV === "prod" || GLYGEN_ENV === "beta") {
-		ReactGA.initialize("UA-123338976-1");
-		ReactGA.pageview(window.location.pathname + window.location.search);
-	}
+  if (GLYGEN_ENV === "prod" || GLYGEN_ENV === "beta" || GLYGEN_ENV === "test") {
+    ReactGA.initialize("UA-123338976-1");
+    ReactGA.pageview(
+      window.location.pathname === Routes.default ? Routes.home : window.location.pathname
+    );
+  }
 }
 
 /**
  * for the pageproofer feedback.
  */
 function pageProofer(d, t) {
-	var url = "";
+  var url = "";
 
-	if (GLYGEN_ENV === "beta") url = "//app.pageproofer.com/overlay/js/3502/1801";
+  if (GLYGEN_ENV === "beta") url = "//app.pageproofer.com/overlay/js/3502/1801";
 
-	if (GLYGEN_ENV === "test") url = "//app.pageproofer.com/overlay/js/3487/1801";
+  if (GLYGEN_ENV === "test") url = "//app.pageproofer.com/overlay/js/3487/1801";
 
-	if (GLYGEN_ENV === "beta" || GLYGEN_ENV === "test") {
-		var pp = d.createElement(t),
-			s = d.getElementsByTagName(t)[0];
-		pp.src = url;
-		pp.type = "text/javascript";
-		pp.async = true;
-		s.parentNode.insertBefore(pp, s);
-	}
+  if (GLYGEN_ENV === "beta" || GLYGEN_ENV === "test") {
+    var pp = d.createElement(t),
+      s = d.getElementsByTagName(t)[0];
+    pp.src = url;
+    pp.type = "text/javascript";
+    pp.async = true;
+    s.parentNode.insertBefore(pp, s);
+  }
 }
 
 const theme = createMuiTheme({
-	typography: {
-		fontFamily: [
-			"-apple-system",
-			"BlinkMacSystemFont",
-			'"Segoe UI"',
-			"Roboto",
-			"Oxygen",
-			"Ubuntu",
-			"Cantarell",
-			'"Fira Sans"',
-			'"Droid Sans"',
-			'"Helvetica Neue"',
-			"sans-serif",
-		].join(","),
-	},
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      '"Fira Sans"',
+      '"Droid Sans"',
+      '"Helvetica Neue"',
+      "sans-serif",
+    ].join(","),
+  },
 });
 
 function App() {
-	initializeReactGA();
-	pageProofer(document, "script");
-	const [userTrackingBannerState, setUserTrackingBannerState] = useState(
-		"none"
-	);
+  initializeReactGA();
+  pageProofer(document, "script");
+  const [userTrackingBannerState, setUserTrackingBannerState] = useState("none");
 
-	return (
-		<div className="App">
-			<ThemeProvider theme={theme}>
-				<Header
-					userTrackingBannerState={userTrackingBannerState}
-					setUserTrackingBannerState={setUserTrackingBannerState}
-				/>
-				<Routes
-					userTrackingBannerState={userTrackingBannerState}
-					setUserTrackingBannerState={setUserTrackingBannerState}
-				/>
-				<Footer />
-			</ThemeProvider>
-		</div>
-	);
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <Header
+          userTrackingBannerState={userTrackingBannerState}
+          setUserTrackingBannerState={setUserTrackingBannerState}
+        />
+        <Routes
+          userTrackingBannerState={userTrackingBannerState}
+          setUserTrackingBannerState={setUserTrackingBannerState}
+        />
+        <Footer />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
