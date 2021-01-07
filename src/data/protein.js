4f11330555cc0f117edf035f47f397f5c6d87a06
RupaliM <rupali.mahadik88@gmail.com>
2021-01-07 17:30:57 -0500

    sitepage

--- src/data/protein.js
+++ src/data/protein.js
@@ -32,13 +32,16 @@ export const getProteinList = (
 };
 
 export const getProteinsiteDetail = (protienId, position) => {
-  const queryParamString = JSON.stringify({
-    uniprot_canonical_ac: protienId,
-    start_pos: parseInt(position),
-    end_pos: parseInt(position)
-  });
-  const url = `/proteinsite/detail?query=${queryParamString}`;
+  // const queryParamString = JSON.stringify({
+  //   uniprot_canonical_ac: protienId,
+  //   start_pos: parseInt(position),
+  //   end_pos: parseInt(position)
+  // });
+
+  const url = `/site/detail/${protienId}.${position}.${position}`;
   return getJson(url);
+  // const url = `/proteinsite/detail?query=${queryParamString}`;
+  // return getJson(url);
 };
 
 export const getProteinDetail = accessionId => {
