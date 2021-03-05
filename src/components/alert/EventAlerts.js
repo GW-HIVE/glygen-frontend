0712843137ed36c91ee4e197f43a0a535a0bcb01
Tatiana <tbw32@hotmail.com>
2021-03-05 14:57:36 -0500

    Event Alert Update

--- src/components/alert/EventAlerts.js
+++ src/components/alert/EventAlerts.js
@@ -13,48 +13,53 @@ export default function EventAlerts(props) {
 
   return (
     <div>
-      <Collapse in={open} className={style}>
-        <Card>
-          <CardLoader pageLoading={props.pageLoading} />
-          <Alert
-            classes={{
-              message: "alert-banner-message",
-              icon: "alert-banner-icon gg-align-middle",
-            }}
-            severity="info"
-            action={
-              <IconButton
-                aria-label="close"
-                // color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpen(false);
-                  setStyle(!style);
-                }}
-              >
-                <CloseIcon fontSize="inherit" className="gg-blue-color" />
-              </IconButton>
-            }
-          >
-            <Row>
-              {props.data.map((obj) => (
+      {props.data.map((obj) => (
+        <Collapse in={open} className={style}>
+          <Card>
+            <CardLoader pageLoading={props.pageLoading} />
+            <Alert
+              classes={{
+                message: "alert-banner-message",
+                icon: "alert-banner-icon gg-align-middle",
+              }}
+              severity="info"
+              action={
+                <IconButton
+                  aria-label="close"
+                  // color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setOpen(false);
+                    setStyle(!style);
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" className="gg-blue-color" />
+                </IconButton>
+              }
+            >
+              <Row>
+                {/* {props.data.map((obj) => ( */}
                 <Col xs={12} sm={"auto"} className={"mt-1 mb-1"}>
                   <>
                     <AlertTitle>
                       <h5 className={"gg-blue-color"}>{obj.title}</h5>
                     </AlertTitle>
-                    {obj.description} {obj.start_date} - {obj.end_date}{" "}
+                    <div>
+                      {obj.start_date} - {obj.end_date}{" "}
+                    </div>{" "}
+                    {obj.description}
                     <a href={obj.url} target="_blank" rel="noopener noreferrer">
                       <span className="gg-link">{obj.url_name}</span>{" "}
                     </a>{" "}
                     {obj.venue}
                   </>
                 </Col>
-              ))}
-            </Row>
-          </Alert>
-        </Card>
-      </Collapse>
+                {/* ))} */}
+              </Row>
+            </Alert>
+          </Card>
+        </Collapse>
+      ))}
     </div>
   );
 }
