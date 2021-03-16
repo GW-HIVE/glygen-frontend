8ede554e120598dbc05e7d7910a8ce91ae2865e5
Tatiana <tbw32@hotmail.com>
2021-03-15 20:40:53 -0400

    Added motif synonym to list of motifs

--- src/data/motif.js
+++ src/data/motif.js
@@ -74,10 +74,7 @@ export const MOTIF_LIST_COLUMNS = [
     ),
     headerStyle: (colum, colIndex) => {
       return {
-        // width: "35%",
-        textAlign: "left",
-        backgroundColor: "#4B85B6",
-        color: "white",
+        // width: "20%",
         whiteSpace: "nowrap",
       };
     },
@@ -85,11 +82,10 @@ export const MOTIF_LIST_COLUMNS = [
   {
     dataField: "motif_ac",
     text: motifStrings.motif_id.name,
-
     sort: true,
     selected: true,
     headerStyle: () => {
-      return { backgroundColor: "#4B85B6", color: "white", width: "20%" };
+      return { width: "20%" };
     },
     formatter: (value, row) => (
       <LineTooltip text="View details">
@@ -102,7 +98,7 @@ export const MOTIF_LIST_COLUMNS = [
     text: motifStrings.motif_name.name,
     sort: true,
     headerStyle: (colum, colIndex) => {
-      return { backgroundColor: "#4B85B6", color: "white", width: "20%" };
+      return { width: "20%" };
     },
     formatter: (value, row) => (
       <LineTooltip text="View details">
@@ -111,11 +107,24 @@ export const MOTIF_LIST_COLUMNS = [
     ),
   },
   {
+    dataField: "synonyms",
+    text: motifStrings.motif_synonym.synonym,
+    sort: false,
+    headerStyle: (colum, colIndex) => {
+      return { width: "20%" };
+    },
+    formatter: (value, row) => (
+      <LineTooltip text="View details">
+        <Link to={routeConstants.motifDetail + row.motif_ac}>{row.synonyms}</Link>
+      </LineTooltip>
+    ),
+  },
+  {
     dataField: "glycan_count",
     text: motifStrings.glycan_count.name,
     sort: true,
     headerStyle: (colum, colIndex) => {
-      return { backgroundColor: "#4B85B6", color: "white", width: "20%" };
+      return { width: "20%" };
     },
     formatter: (value, row) => (
       <LineTooltip text="View details">
