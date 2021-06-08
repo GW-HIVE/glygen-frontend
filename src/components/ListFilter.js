6caf1ddbffeb8aacf01f9b270e2cacb047ee33e9
RupaliM <rupali.mahadik88@gmail.com>
2021-06-08 17:58:48 -0400

    Lifting State Up

--- src/components/ListFilter.js
+++ src/components/ListFilter.js
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { Checkbox, Collapse } from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
-import { Row, Col, div } from "react-bootstrap";
+// import CloseIcon from "@material-ui/icons/Close";
+// import { Row, Col, div } from "react-bootstrap";
 import "../css/detail.css";
-import proteinSearchData from "../data/json/proteinSearch";
-import SelectControl from "../components/select/SelectControl";
-import Iframe from "react-iframe";
+// import proteinSearchData from "../data/json/proteinSearch";
+// import SelectControl from "../components/select/SelectControl";
+// import Iframe from "react-iframe";
 import { withStyles } from "@material-ui/core/styles";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 const BlueCheckbox = withStyles({
   root: {
     color: "#979797",
     "&$checked": {
-      color: "#2f78b7",
-    },
+      color: "#2f78b7"
+    }
   },
-  checked: {},
-})((props) => <Checkbox color="default" {...props} />);
+  checked: {}
+})(props => <Checkbox color="default" {...props} />);
 
-let advancedSearch = proteinSearchData.advanced_search;
-const ListFilterOptionGroup = ({ type, onFilterChange }) => {
-  const [optionState, setOptionState] = useState(type.options);
-  const [annotationOperation, setAnnotationOperation] = useState(type.operator || "OR");
+// let advancedSearch = proteinSearchData.advanced_search;
+const ListFilterOptionGroup = props => {
+  const { type, onFilterChange } = props;
+  const [optionState, setOptionState] = useState([...type.options]);
+  const [selection, setSelection] = useState(null);
+  const [annotationOperation, setAnnotationOperation] = useState(
+    type.operator || "OR"
+  );
   // sort by order field
   optionState.sort((a, b) => {
     if (a.order < b.order) return -1;
@@ -29,11 +33,15 @@ const ListFilterOptionGroup = ({ type, onFilterChange }) => {
     return 0;
   });
 
-  const handleOptionChange = (event) => {
+  // useEffect(() => {
+  //   setOptionState([...props.type.options]);
+  // }, [props.type.options]);
+
+  const handleOptionChange = event => {
     // get the value (which option) and checked state
     const { checked, value } = event.target;
     const newOptionState = [...optionState];
-    newOptionState.find((item) => item.id === value).selected = checked;
+    newOptionState.find(item => item.id === value).selected = checked;
     setOptionState(newOptionState);
     // if (newOptionState) {
     //   setAnnotationOperation();
@@ -44,14 +52,20 @@ const ListFilterOptionGroup = ({ type, onFilterChange }) => {
     if (!(annotationOperation && optionState)) {
       return;
     }
+    const selectedOptions = optionState
+      .filter(item => item.selected)
+      .map(item => item.id);
 
     const filter = {
       id: type.id,
       operator: annotationOperation,
-      selected: optionState.filter((item) => item.selected).map((item) => item.id),
+      selected: selectedOptions
     };
 
-    onFilterChange(filter);
+    if (selectedOptions !== selection) {
+      setSelection(selectedOptions);
+      onFilterChange(filter);
+    }
   }, [annotationOperation, optionState]);
 
   return (
@@ -63,7 +77,7 @@ const ListFilterOptionGroup = ({ type, onFilterChange }) => {
           <select
             className="select-dropdown float-right pt-0"
             value={annotationOperation}
-            onChange={(event) => setAnnotationOperation(event.target.value)}
+            onChange={event => setAnnotationOperation(event.target.value)}
           >
             <option value="OR">OR</option>
             <option value="AND">AND</option>
@@ -71,22 +85,23 @@ const ListFilterOptionGroup = ({ type, onFilterChange }) => {
         </div>
         {/* <div className="parentElement">{type.label}</div> */}
         <ul className="list-unstyled mt-0 mb-0 pt-1">
-          {optionState.map((option) => (
-            <li key={option.id}>
-              <FormControlLabel
-                className="pl-2 mt-0 mb-0 pt-0 pb-0"
-                control={
-                  <BlueCheckbox
-                    value={option.id}
-                    checked={option.selected}
-                    onChange={handleOptionChange}
-                    size="small"
-                    className="pt-1 pb-1"
-                  />
-                }
-                label={`${option.label} (${option.count})`}
-              />
-              {/* <BlueCheckbox
+          {type.options &&
+            type.options.map(option => (
+              <li key={option.id}>
+                <FormControlLabel
+                  className="pl-2 mt-0 mb-0 pt-0 pb-0"
+                  control={
+                    <BlueCheckbox
+                      value={option.id}
+                      checked={option.selected}
+                      onChange={handleOptionChange}
+                      size="small"
+                      className="pt-1 pb-1"
+                    />
+                  }
+                  label={`${option.label} (${option.count})`}
+                />
+                {/* <BlueCheckbox
                 // className="checkboxCss"
                 value={option.id}
                 checked={option.selected}
@@ -95,40 +110,48 @@ const ListFilterOptionGroup = ({ type, onFilterChange }) => {
               <label>
                 {option.label}{" "}({option.count})
               </label> */}
-            </li>
-          ))}
+              </li>
+            ))}
         </ul>
       </div>
     </>
   );
 };
 
-const ListFilter = ({ availableOptions = [], selectedOptions = [], onFilterChange }) => {
+const ListFilter = ({
+  availableOptions = [],
+  selectedOptions = [],
+  onFilterChange
+}) => {
   // If nothing available, exit
   if (!availableOptions.length) {
     return <></>;
   }
 
   // create new array, holding integrated available / selected data
-  const filterGroupData = availableOptions.map((group) => {
+  const filterGroupData = availableOptions.map(group => {
     // See if there is an entry in the selected values for this type
-    const selectGroup = selectedOptions.find((selected) => selected.id === group.id);
+    const selectGroup = selectedOptions.find(
+      selected => selected.id === group.id
+    );
 
     // create a return that is the same except for selection
     return {
       ...group,
-      options: group.options.map((option) => ({
+      options: group.options.map(option => ({
         ...option,
         // false if no matching group in selection, or see if its in the selection
-        selected: selectGroup ? selectGroup.selected.indexOf(option.id) > -1 : false,
-      })),
+        selected: selectGroup
+          ? selectGroup.selected.indexOf(option.id) > -1
+          : false
+      }))
     };
   });
 
   return (
     <div className="parentlist">
-      {filterGroupData.map((type) => (
-        <div>
+      {filterGroupData.map(type => (
+        <div key={type.id}>
           <ListFilterOptionGroup type={type} onFilterChange={onFilterChange} />
         </div>
       ))}
